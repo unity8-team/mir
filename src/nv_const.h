3f140052f891a4dc674b82e438c9595e59b4676b
Aaron Plattner <aplattner@nvidia.com>
2006-03-27 21:40:24 +0000

    Bump version to 1.0.2.0 for release.

--- src/nv_const.h
+++ src/nv_const.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nv/nv_const.h,v 1.7 2003/07/31 20:24:29 mvojkovi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nv/nv_const.h,v 1.6 2001/12/07 00:09:55 mvojkovi Exp $ */
 
 #ifndef __NV_CONST_H__
 #define __NV_CONST_H__
@@ -8,7 +8,7 @@
 #define NV_DRIVER_NAME "nv"
 #define NV_MAJOR_VERSION 1
 #define NV_MINOR_VERSION 0
-#define NV_PATCHLEVEL 1
+#define NV_PATCHLEVEL 2
 
 #endif /* __NV_CONST_H__ */
           
