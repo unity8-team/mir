e3d290e37f775b6325e086eec21ab2706eb5b04d
Eric Anholt <anholt@FreeBSD.org>
2006-04-06 15:57:12 -0700

    Fix prototype for i830DetectCRT.

--- src/i830_display.h
+++ src/i830_display.h
@@ -1,4 +1,4 @@
 Bool i830SetMode(ScrnInfoPtr pScrn, DisplayModePtr pMode);
-Bool i830DetectCRT(ScreenPtr pScrn);
+Bool i830DetectCRT(ScrnInfoPtr pScrn);
 void i830SetLVDSPanelPower(ScrnInfoPtr pScrn, Bool on);
 void i830PipeSetBase(ScrnInfoPtr pScrn, int pipe, int x, int y);
