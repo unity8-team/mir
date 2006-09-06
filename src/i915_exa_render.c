3ae04c66518291a3b8a005dd97400450321659ee
Eric Anholt <eric@anholt.net>
2006-09-06 11:15:17 -0700

    Remove a debug printf.

--- src/i915_exa_render.c
+++ src/i915_exa_render.c
@@ -330,8 +330,6 @@ I915EXAPrepareComposite(int op, PicturePtr pSrcPicture,
     CARD32 dst_format, dst_offset, dst_pitch;
     CARD32 blendctl;
 
-    ErrorF("i915 prepareComposite\n");
-
     I915GetDestFormat(pDstPicture, &dst_format);
     dst_offset = exaGetPixmapOffset(pDst);
     dst_pitch = exaGetPixmapPitch(pDst);
