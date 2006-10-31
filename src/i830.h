2fe0e4fb10fdb1b0db90cdba158675c000532d46
Eric Anholt <eric@anholt.net>
2006-10-31 15:00:36 -0800

    Add compat definitions for M_T_PREFERRED and M_T_DRIVER for older X Servers.

--- src/i830.h
+++ src/i830.h
@@ -724,4 +724,12 @@ void i830_tv_init(ScrnInfoPtr pScrn);
 #define _845_DRAM_RW_CONTROL 0x90
 #define DRAM_WRITE    0x33330000
 
+/* Compat definitions for older X Servers. */
+#ifndef M_T_PREFERRED
+#define M_T_PREFERRED	0x08
+#endif
+#ifndef M_T_DRIVER
+#define M_T_DRIVER	0x40
+#endif
+
 #endif /* _I830_H_ */
