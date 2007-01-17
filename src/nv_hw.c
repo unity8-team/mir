7e12e93c2b11c5d784922264ef9822ca178ecb2c
Jeremy Kolb <jkolb@brandeis.edu>
2007-01-17 08:35:27 -0500

    Fix PGRAPH_DEBUG* regs to match what's done on the nv35. Specifically change DEBUG_3 | 0x00de0000 for data/format/dma checking. This is consistent with other cards and dumps so it should be correct.

--- src/nv_hw.c
+++ src/nv_hw.c
@@ -1135,13 +1135,13 @@ void NVLoadStateExt (
               nvWriteGRAPH(pNv, 0x0b3c, 0x00006000);
            } else
            if(pNv->Architecture == NV_ARCH_30) {
-              nvWriteGRAPH(pNv, NV_PGRAPH_DEBUG_1, 0x40108700);
+              nvWriteGRAPH(pNv, NV_PGRAPH_DEBUG_1, 0x401287c0);
               nvWriteGRAPH(pNv, 0x0890, 0x00140000);
-              nvWriteGRAPH(pNv, NV_PGRAPH_DEBUG_3, 0xf00e0431);
-              nvWriteGRAPH(pNv, NV_PGRAPH_DEBUG_4, 0x00008000);
+              nvWriteGRAPH(pNv, NV_PGRAPH_DEBUG_3, 0xf0de0475);
+              nvWriteGRAPH(pNv, NV_PGRAPH_DEBUG_4, 0x10008000);
               nvWriteGRAPH(pNv, NV_PGRAPH_LIMIT_VIOL_PIX, 0xf04b1f36);
-              nvWriteGRAPH(pNv, 0x0B80, 0x1002d888);
-              nvWriteGRAPH(pNv, 0x0B88, 0x62ff007f);
+              nvWriteGRAPH(pNv, 0x0B80, 0x1003d888);
+              nvWriteGRAPH(pNv, 0x0B88, 0x62ff0f7f);
            } else {
               nvWriteGRAPH(pNv, 0x0084, 0x00118700);
               nvWriteGRAPH(pNv, 0x008C, 0xF20E0431);
