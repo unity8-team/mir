7fb00d3e6ab38c4989d9c65c86cd9019d8d7af94
Andrew Randrianasulu <randrik@mail.ru>
2007-06-22 00:55:00 -0400

    RADEON: Fix VT switch hangs
    - reorder RADEONDRISetVBlankInterrupt() and RADEONDRIResume()
    - see bug 11287

--- src/radeon_driver.c
+++ src/radeon_driver.c
@@ -6931,10 +6931,9 @@ Bool RADEONEnterVT(int scrnIndex, int flags)
     	}
 
 	/* get the DRI back into shape after resume */
+	RADEONDRISetVBlankInterrupt (pScrn, TRUE);
 	RADEONDRIResume(pScrn->pScreen);
 	RADEONAdjustMemMapRegisters(pScrn, &info->ModeReg);
-
-	RADEONDRISetVBlankInterrupt (pScrn, TRUE);
     }
 #endif
     /* this will get XVideo going again, but only if XVideo was initialised
