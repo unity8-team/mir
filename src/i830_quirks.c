a00e40100fc30d321409a832413989fdfd3ae6aa
Rob <rglowery@exemail.com.au>
2007-10-17 09:19:19 +0800

    AOpen 965GM mini pc has no LVDS

--- src/i830_quirks.c
+++ src/i830_quirks.c
@@ -58,7 +58,8 @@ static void quirk_mac_mini (I830Ptr pI830)
 static i830_quirk i830_quirk_list[] = {
     /* Aopen mini pc */
     { PCI_CHIP_I945_GM, 0xa0a0, SUBSYS_ANY, quirk_ignore_lvds },
-    
+    { PCI_CHIP_I965_GM, 0x8086, 0x1999, quirk_ignore_lvds },
+
     /* Apple Mac mini has no lvds, but macbook pro does */
     { PCI_CHIP_I945_GM, 0x8086, 0x7270, quirk_mac_mini },
     
