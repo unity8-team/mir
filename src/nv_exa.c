5b31c50adecf0c3b5a76cfa61ef0d40be9ccf25d
Stephane Marchesin <marchesin@icps.u-strasbg.fr>
2007-10-30 01:49:38 +0100

    Disable nv30 exa no big endian until we fix the endianness bugs related to the 3D engine.

--- src/nv_exa.c
+++ src/nv_exa.c
@@ -691,15 +691,13 @@ NVExaInit(ScreenPtr pScreen)
  		pNv->EXADriverPtr->DoneComposite    = NV10DoneComposite;
 		break;
 		
-#if defined(ENABLE_NV30EXA)
+#if (X_BYTE_ORDER == X_LITTLE_ENDIAN) && defined(ENABLE_NV30EXA)
 	case NV_ARCH_30:
 		pNv->EXADriverPtr->CheckComposite   = NV30EXACheckComposite;
 		pNv->EXADriverPtr->PrepareComposite = NV30EXAPrepareComposite;
 		pNv->EXADriverPtr->Composite        = NV30EXAComposite;
 		pNv->EXADriverPtr->DoneComposite    = NV30EXADoneComposite;
 		break;
-#endif
-#if (X_BYTE_ORDER == X_LITTLE_ENDIAN) && defined(ENABLE_NV30EXA)
 	case NV_ARCH_40:
 		pNv->EXADriverPtr->CheckComposite   = NV40EXACheckComposite;
 		pNv->EXADriverPtr->PrepareComposite = NV40EXAPrepareComposite;
