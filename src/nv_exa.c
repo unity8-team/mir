334b98da1efacaf86d044e02b344dd03b53cd5b2
Patrice Mandin <pmandin@caramail.com>
2007-11-09 18:12:10 +0100

    Also disable nv40exa if nv30exa disabled

--- src/nv_exa.c
+++ src/nv_exa.c
@@ -710,12 +710,14 @@ NVExaInit(ScreenPtr pScreen)
 		pNv->EXADriverPtr->DoneComposite    = NV30EXADoneComposite;
 		break;
 #endif
+#if defined(ENABLE_NV30EXA)	
 	case NV_ARCH_40:
 		pNv->EXADriverPtr->CheckComposite   = NV40EXACheckComposite;
 		pNv->EXADriverPtr->PrepareComposite = NV40EXAPrepareComposite;
 		pNv->EXADriverPtr->Composite        = NV40EXAComposite;
 		pNv->EXADriverPtr->DoneComposite    = NV40EXADoneComposite;
 		break;
+#endif
 	case NV_ARCH_50:
 		break;
 	default:
