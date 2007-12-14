4720fda517a518adf3fa3123dc8af5595093d306
Alex Deucher (none) <alex@botch2.>
2007-12-14 02:17:14 -0500

    RADEON: select fb_div0 for LVDS on RV410 (x700) mobility
    Fixes bug 8038
    I wonder if desktop RV410 need a similar fix.
    If your x700 laptop panel has problems after this let me know.

--- src/radeon_driver.c
+++ src/radeon_driver.c
@@ -4038,6 +4038,10 @@ void RADEONRestoreLVDSRegisters(ScrnInfoPtr pScrn, RADEONSavePtr restore)
     if (info->IsMobility) {
 	OUTREG(RADEON_LVDS_GEN_CNTL,  restore->lvds_gen_cntl);
 	OUTREG(RADEON_LVDS_PLL_CNTL,  restore->lvds_pll_cntl);
+
+	if (info->ChipFamily == CHIP_FAMILY_RV410) {
+	    OUTREG(RADEON_CLOCK_CNTL_INDEX, 0);
+	}
     }
 
 }
