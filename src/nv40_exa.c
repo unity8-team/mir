5ba918b6828af765458a95304157e2b870bde69e
Ben Skeggs <skeggsb@gmail.com>
2007-12-15 23:45:58 +1100

    nv40: just for stillunknown...
    Fix composite transforms.  This is getting to a point where it may be
    worth doing this in the vertex program perhaps.  For now this works,
    it'll do.

--- src/nv40_exa.c
+++ src/nv40_exa.c
@@ -513,13 +513,12 @@ NV40EXAPrepareComposite(int op, PicturePtr psPict,
 #define xFixedToFloat(v) \
 	((float)xFixedToInt((v)) + ((float)xFixedFrac(v) / 65536.0))
 
-static void
+static inline void
 NV40EXATransformCoord(PictTransformPtr t, int x, int y, float sx, float sy,
 					  float *x_ret, float *y_ret)
 {
-	PictVector v;
-
 	if (t) {
+		PictVector v;
 		v.vector[0] = IntToxFixed(x);
 		v.vector[1] = IntToxFixed(y);
 		v.vector[2] = xFixed1;
@@ -554,35 +553,52 @@ NV40EXAComposite(PixmapPtr pdPix, int srcX , int srcY,
 {
 	ScrnInfoPtr pScrn = xf86Screens[pdPix->drawable.pScreen->myNum];
 	NVPtr pNv = NVPTR(pScrn);
-	float sX0, sX1, sY0, sY1;
-	float mX0, mX1, mY0, mY1;
+	float sX0, sX1, sX2, sY0, sY1, sY2, sX3, sY3;
+	float mX0, mX1, mX2, mY0, mY1, mY2, mX3, mY3;
 	NV40EXA_STATE;
 
 	NV40EXATransformCoord(state->unit[0].transform, srcX, srcY,
 			      state->unit[0].width,
 			      state->unit[0].height, &sX0, &sY0);
 	NV40EXATransformCoord(state->unit[0].transform,
-			      srcX + width, srcY + height,
+			      srcX + width, srcY,
 			      state->unit[0].width,
 			      state->unit[0].height, &sX1, &sY1);
+	NV40EXATransformCoord(state->unit[0].transform,
+			      srcX + width, srcY + height,
+			      state->unit[0].width,
+			      state->unit[0].height, &sX2, &sY2);
+	NV40EXATransformCoord(state->unit[0].transform,
+			      srcX, srcY + height,
+			      state->unit[0].width,
+			      state->unit[0].height, &sX3, &sY3);
 
 	if (state->have_mask) {
 		NV40EXATransformCoord(state->unit[1].transform, maskX, maskY,
 				      state->unit[1].width,
 				      state->unit[1].height, &mX0, &mY0);
 		NV40EXATransformCoord(state->unit[1].transform,
-				      maskX + width, maskY + height,
+				      maskX + width, maskY,
 				      state->unit[1].width,
 				      state->unit[1].height, &mX1, &mY1);
+		NV40EXATransformCoord(state->unit[1].transform,
+				      maskX + width, maskY + height,
+				      state->unit[1].width,
+				      state->unit[1].height, &mX2, &mY2);
+		NV40EXATransformCoord(state->unit[1].transform,
+				      maskX, maskY + height,
+				      state->unit[1].width,
+				      state->unit[1].height, &mX3, &mY3);
+
 		CV_OUTm(sX0 , sY0 , mX0, mY0, dstX        ,          dstY);
-		CV_OUTm(sX1 , sY0 , mX1, mY0, dstX + width,          dstY);
-		CV_OUTm(sX1 , sY1 , mX1, mY1, dstX + width, dstY + height);
-		CV_OUTm(sX0 , sY1 , mX0, mY1, dstX        , dstY + height);
+		CV_OUTm(sX1 , sY1 , mX1, mY1, dstX + width,          dstY);
+		CV_OUTm(sX2 , sY2 , mX2, mY2, dstX + width, dstY + height);
+		CV_OUTm(sX3 , sY3 , mX3, mY3, dstX        , dstY + height);
 	} else {
 		CV_OUT(sX0 , sY0 , dstX        ,          dstY);
-		CV_OUT(sX1 , sY0 , dstX + width,          dstY);
-		CV_OUT(sX1 , sY1 , dstX + width, dstY + height);
-		CV_OUT(sX0 , sY1 , dstX        , dstY + height);
+		CV_OUT(sX1 , sY1 , dstX + width,          dstY);
+		CV_OUT(sX2 , sY2 , dstX + width, dstY + height);
+		CV_OUT(sX3 , sY3 , dstX        , dstY + height);
 	}
 
 	FIRE_RING();
