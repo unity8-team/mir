ae6dc9954c458f67a2359ead7040693ffbc8c916
Arthur Huillet <arthur.huillet@free.fr>
2007-12-23 13:49:24 +0100

    Revert a manifestation of my stupidity.

--- src/nv_hw.c
+++ src/nv_hw.c
@@ -80,7 +80,6 @@ uint8_t nvReadVGA(NVPtr pNv, uint8_t index)
 void nvWriteVGA(NVPtr pNv, uint8_t index, uint8_t data)
 {
   volatile const uint8_t *ptr = pNv->cur_head ? pNv->PCIO1 : pNv->PCIO0;
-  ErrorF("nvWriteVGA: idx %d data 0x%x\n", index, data);
   VGA_WR08(ptr, 0x03D4, index);
   VGA_WR08(ptr, 0x03D5, data);
 }
@@ -93,7 +92,6 @@ CARD32 nvReadRAMDAC(NVPtr pNv, uint8_t head, uint32_t ramdac_reg)
 
 void nvWriteRAMDAC(NVPtr pNv, uint8_t head, uint32_t ramdac_reg, CARD32 val)
 {
-    ErrorF("nvWriteRAMDAC, head %x, reg 0x%x val %#x\n", head, ramdac_reg, val);
 	volatile const void *ptr = head ? pNv->PRAMDAC1 : pNv->PRAMDAC0;
 	MMIO_OUT32(ptr, ramdac_reg, val);
 }
@@ -106,8 +104,7 @@ CARD32 nvReadCRTC(NVPtr pNv, uint8_t head, uint32_t reg)
 
 void nvWriteCRTC(NVPtr pNv, uint8_t head, uint32_t reg, CARD32 val)
 {
-  ErrorF("nvWriteCRTC head %d, reg 0x%x val %#x\n", head, reg, val);
-    volatile const void *ptr = head ? pNv->PCRTC1 : pNv->PCRTC0;
+  volatile const void *ptr = head ? pNv->PCRTC1 : pNv->PCRTC0;
   MMIO_OUT32(ptr, reg, val);
 }
 
