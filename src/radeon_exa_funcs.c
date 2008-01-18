67be90ff8400227d22846babd10c13cbe6614e97
Alex Deucher (none) <alex@botch2.>
2008-01-18 09:50:38 -0500

    RADEON: make sure EXA Composite is actually disabled on XPRESS chips.

--- src/radeon_exa_funcs.c
+++ src/radeon_exa_funcs.c
@@ -533,10 +533,11 @@ Bool FUNC_NAME(RADEONDrawInit)(ScreenPtr pScreen)
 
 #ifdef RENDER
     if (info->RenderAccel) {
-	if (info->ChipFamily >= CHIP_FAMILY_RV515)
+	if ((info->ChipFamily >= CHIP_FAMILY_RV515) ||
+	    (info->ChipFamily == CHIP_FAMILY_RS400))
 		xf86DrvMsg(pScrn->scrnIndex, X_INFO, "Render acceleration "
-			       "unsupported on R500 and newer cards.\n");
-	else if (IS_R300_VARIANT && !info->IsIGP) {
+			       "unsupported on XPRESS, R500 and newer cards.\n");
+	else if (IS_R300_VARIANT) {
 		xf86DrvMsg(pScrn->scrnIndex, X_INFO, "Render acceleration "
 			       "enabled for R300 type cards.\n");
 		info->exa->CheckComposite = R300CheckComposite;
