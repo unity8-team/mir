d774e597e58af6ad050cae357023b02000275884
Alex Deucher (none) <alex@botch2.>
2008-01-23 18:05:41 -0500

    Revert "RADEON: adjust PAL mode hstart"
    This reverts commit 719a9a376e34d99032af75e3f7b002670ccb816b.
    
    This breaks TV out on some cards.

--- src/radeon_tv.c
+++ src/radeon_tv.c
@@ -161,7 +161,7 @@ static const TVModeConstants availableTVModes[] =
 	TV_STD_PAL,        /* standard */
 	1144,              /* horTotal */
 	706,               /* verTotal */
-	799,               /* horStart */
+	812,               /* horStart */
 	824,               /* horSyncStart */
 	669,               /* verSyncStart */
 	696700,            /* defRestart */
