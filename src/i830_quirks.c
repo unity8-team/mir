4ee3f6482c36b80d8bb1faf57cb14390571761f0
Andreas Stawinoga <a.stawinoga@gmx.de>
2008-01-24 08:51:09 +0800

    Samsung Q45 has no TV output

--- src/i830_quirks.c
+++ src/i830_quirks.c
@@ -91,6 +91,8 @@ static i830_quirk i830_quirk_list[] = {
 
     /* Samsung Q35 has no TV output */
     { PCI_CHIP_I945_GM, 0x144d, 0xc504, quirk_ignore_tv },
+    /* Samsung Q45 has no TV output */
+    { PCI_CHIP_I965_GM, 0x144d, 0xc510, quirk_ignore_tv },
 
     /* Dell Inspiron 510m needs pipe A force quirk */
     { PCI_CHIP_I855_GM, 0x1028, 0x0164, quirk_pipea_force },
