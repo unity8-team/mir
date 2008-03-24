7cbbf852b2606a396924ac516776ff2e772b2edb
Alex Deucher (none) <alex@samba.>
2008-03-24 13:04:57 -0400

    R3xx/R5xx: use non VAP/TCP for textured video
    Just extra state to emit.

--- src/radeon_textured_videofuncs.c
+++ src/radeon_textured_videofuncs.c
@@ -127,10 +127,10 @@ FUNC_NAME(RADEONDisplayTexturedVideo)(ScrnInfoPtr pScrn, RADEONPortPrivPtr pPriv
     FINISH_VIDEO();
 
     if (IS_R300_VARIANT || IS_AVIVO_VARIANT) {
-	int has_tcl = ((info->ChipFamily != CHIP_FAMILY_RS690) &&
+	int has_tcl = 0; /*((info->ChipFamily != CHIP_FAMILY_RS690) &&
 		       (info->ChipFamily != CHIP_FAMILY_RS740) &&
 		       (info->ChipFamily != CHIP_FAMILY_RS400) &&
-		       (info->ChipFamily != CHIP_FAMILY_RV515));
+		       (info->ChipFamily != CHIP_FAMILY_RV515));*/
 
 	switch (pPixmap->drawable.bitsPerPixel) {
 	case 16:
