69fe4b77181c33cca69143bd4f15dfa3be170577
Jesse Barnes <jbarnes@virtuousgeek.org>
2008-09-22 13:26:58 -0700

    Add no TV out quirk for HP Compaq nx6110
    Hopefully we can fix the VBT parsing code so we don't need this, but for the
    time being it seems necessary.  Fixes bz #17683.

--- src/i830_quirks.c
+++ src/i830_quirks.c
@@ -281,6 +281,8 @@ static i830_quirk i830_quirk_list[] = {
     /* Samsung Q45 has no TV output */
     { PCI_CHIP_I965_GM, 0x144d, 0xc510, quirk_ignore_tv },
 
+    /* HP Compaq nx6110 has no TV output */
+    { PCI_CHIP_I915_GM, 0x103c, 0x099c, quirk_ignore_tv },
     /* HP Compaq 6730s has no TV output */
     { PCI_CHIP_GM45_GM, 0x103c, 0x30e8, quirk_ignore_tv },
 
