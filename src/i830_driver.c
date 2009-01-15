8f1295c086837ed2f3e60ecc1ce6d6ba5ee7abd2
Owain G. Ainsworth <oga@openbsd.org>
2009-01-14 17:12:33 -0800

    use ifdef __linux__ where needed.
    since modesetting is compiled by default now, ifdef __linux__ the linux
    only includes and ioctls.

--- src/i830_driver.c
+++ src/i830_driver.c
@@ -1228,7 +1228,9 @@ i830SetHotkeyControl(ScrnInfoPtr pScrn, int mode)
  * DRM mode setting Linux only at this point... later on we could
  * add a wrapper here.
  */
+#ifdef __linux__
 #include <linux/kd.h>
+#endif
 
 static Bool i830_kernel_mode_enabled(ScrnInfoPtr pScrn)
 {
@@ -1254,7 +1256,9 @@ static Bool i830_kernel_mode_enabled(ScrnInfoPtr pScrn)
     if (ret)
 	return FALSE;
 
+#ifdef __linux__
     ioctl(xf86Info.consoleFd, KDSETMODE, KD_TEXT);
+#endif
 
     return TRUE;
 }
