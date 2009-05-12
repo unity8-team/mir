raboof/xf86-video-nouveau

commit 3971dda57004894d5d4fc9420aa00da400815af9

randr12: fix fp_control again again again
As prophesied, 9c991d3a84ad16f893b739282403eb01e02bb57f broke something:
turning off a digital output, then turning on an analogue output on the
same crtc as previously used by the digital output led to FP_TG_CONTROL
being turned on again when the (unmodified by analogue mode_set) fp_control
state was written out

So, belt and braces attempt to get this right, one more time

src/nv_output.c
@@ -669,16 +669,19 @@ nv_output_prepare(xf86OutputPtr output)
 
 	output->funcs->dpms(output, DPMSModeOff);
 
-	if (nv_encoder->dcb->type == OUTPUT_ANALOG &&
-	    NVReadRAMDAC(pNv, head, NV_PRAMDAC_FP_TG_CONTROL) &
-			 FP_TG_CONTROL_ON) {
-		/* digital remnants must be cleaned before new crtc values
-		 * programmed.  delay is time for the vga stuff to realise it's
-		 * in control again
-		 */
-		NVWriteRAMDAC(pNv, head, NV_PRAMDAC_FP_TG_CONTROL,
-			      FP_TG_CONTROL_OFF);
-		usleep(50000);
+	if (nv_encoder->dcb->type == OUTPUT_ANALOG) {
+		if (NVReadRAMDAC(pNv, head, NV_PRAMDAC_FP_TG_CONTROL) &
+							FP_TG_CONTROL_ON) {
+			/* digital remnants must be cleaned before new crtc
+			 * values programmed.  delay is time for the vga stuff
+			 * to realise it's in control again
+			 */
+			NVWriteRAMDAC(pNv, head, NV_PRAMDAC_FP_TG_CONTROL,
+				      FP_TG_CONTROL_OFF);
+			usleep(50000);
+		}
+		/* don't inadvertently turn it on when state written later */
+		crtcstate[head].fp_control = FP_TG_CONTROL_OFF;
 	}
 
 	/* calculate some output specific CRTC regs now, so that they can be
@@ -744,16 +747,17 @@ nv_output_mode_set(xf86OutputPtr output, DisplayModePtr mode, DisplayModePtr adj
 		run_tmds_table(pScrn, dcbe, head, adjusted_mode->Clock);
 	else if (dcbe->type == OUTPUT_LVDS)
 		call_lvds_script(pScrn, dcbe, head, LVDS_RESET, adjusted_mode->Clock);
+	if (dcbe->type == OUTPUT_LVDS || dcbe->type == OUTPUT_TMDS)
+		/* update fp_control state for any changes made by scripts,
+		 * so correct value is written at DPMS on */
+		pNv->ModeReg.crtc_reg[head].fp_control =
+			NVReadRAMDAC(pNv, head, NV_PRAMDAC_FP_TG_CONTROL);
 
 	/* This could use refinement for flatpanels, but it should work this way */
 	if (pNv->NVArch < 0x44)
 		NVWriteRAMDAC(pNv, 0, NV_PRAMDAC_TEST_CONTROL + nv_output_ramdac_offset(nv_encoder), 0xf0000000);
 	else
 		NVWriteRAMDAC(pNv, 0, NV_PRAMDAC_TEST_CONTROL + nv_output_ramdac_offset(nv_encoder), 0x00100000);
-
-	/* update fp_control state for any changes made by scripts, for dpms */
-	pNv->ModeReg.crtc_reg[head].fp_control =
-			NVReadRAMDAC(pNv, head, NV_PRAMDAC_FP_TG_CONTROL);
 }
 
 static void
@@ -773,28 +777,37 @@ static void dpms_update_fp_control(ScrnInfoPtr pScrn, struct nouveau_encoder *nv
 {
 	NVPtr pNv = NVPTR(pScrn);
 	struct nouveau_crtc *nv_crtc;
-	uint32_t fpc;
+	uint32_t *fpc;
 	xf86CrtcConfigPtr xf86_config = XF86_CRTC_CONFIG_PTR(pScrn);
 	int i;
 
 	if (mode == DPMSModeOn) {
 		nv_crtc = to_nouveau_crtc(crtc);
-		fpc = pNv->ModeReg.crtc_reg[nv_crtc->head].fp_control;
+		fpc = &pNv->ModeReg.crtc_reg[nv_crtc->head].fp_control;
+
+		if ((*fpc & (FP_TG_CONTROL_ON | FP_TG_CONTROL_OFF)) ==
+							FP_TG_CONTROL_OFF)
+			/* using saved value is ok, as (is_digital && dpms_on &&
+			 * fp_control==OFF) is (at present) *only* true when
+			 * fpc's most recent change was by below "off" code
+			 */
+			*fpc = nv_crtc->dpms_saved_fp_control;
 
 		nv_crtc->fp_users |= 1 << nv_encoder->dcb->index;
-		NVWriteRAMDAC(pNv, nv_crtc->head, NV_PRAMDAC_FP_TG_CONTROL, fpc);
+		NVWriteRAMDAC(pNv, nv_crtc->head, NV_PRAMDAC_FP_TG_CONTROL, *fpc);
 	} else
 		for (i = 0; i < xf86_config->num_crtc; i++) {
 			nv_crtc = to_nouveau_crtc(xf86_config->crtc[i]);
-			fpc = pNv->ModeReg.crtc_reg[nv_crtc->head].fp_control;
+			fpc = &pNv->ModeReg.crtc_reg[nv_crtc->head].fp_control;
 
 			nv_crtc->fp_users &= ~(1 << nv_encoder->dcb->index);
 			if (!nv_crtc->fp_users) {
+				nv_crtc->dpms_saved_fp_control = *fpc;
 				/* cut the FP output */
-				fpc &= ~FP_TG_CONTROL_ON;
-				fpc |= FP_TG_CONTROL_OFF;
+				*fpc &= ~FP_TG_CONTROL_ON;
+				*fpc |= FP_TG_CONTROL_OFF;
 				NVWriteRAMDAC(pNv, nv_crtc->head,
-					      NV_PRAMDAC_FP_TG_CONTROL, fpc);
+					      NV_PRAMDAC_FP_TG_CONTROL, *fpc);
 			}
 		}
 }

src/nv_type.h
@@ -213,6 +213,7 @@ struct nouveau_crtc {
 	uint8_t last_dpms;
 	ExaOffscreenArea *shadow;
 	int fp_users;
+	uint32_t dpms_saved_fp_control;
 	int saturation, sharpness;
 };
 

unity8-team/mir

commit 1b3916d50d0fdcdb5fb5899a50014ce78fad205c

randr12: fix digital dpms regression
introduced in 3971dda57004894d5d4fc9420aa00da400815af9

setting two different "blank" DPMS modes (say Suspend, followed by Off)
consecutively would lead to the saved fp control value getting overwritten
with FP_TG_CONTROL_OFF flavoured state, so DPMS on would then never work

also, an improvement to LVDS DPMS state caching

src/nv_crtc.c
@@ -182,7 +182,7 @@ nv_crtc_dpms(xf86CrtcPtr crtc, int mode)
 	/* nv4ref indicates these two RPC1 bits inhibit h/v sync */
 	crtc1A = NVReadVgaCrtc(pNv, nv_crtc->head, NV_CIO_CRE_RPC1_INDEX) & ~0xC0;
 	switch(mode) {
-		case DPMSModeStandby:
+	case DPMSModeStandby:
 		/* Screen: Off; HSync: Off, VSync: On -- Not Supported */
 		seq1 = 0x20;
 		crtc17 = 0x80;

src/nv_output.c
@@ -831,6 +831,12 @@ static void nv_digital_output_prepare_sel_clk(NVPtr pNv, struct nouveau_encoder 
 			   NV_PRAMDAC_FP_TG_CONTROL_HSYNC_DISABLE |	\
 			   NV_PRAMDAC_FP_TG_CONTROL_VSYNC_DISABLE)
 
+static bool is_fpc_off(uint32_t fpc)
+{
+	return ((fpc & (FP_TG_CONTROL_ON | FP_TG_CONTROL_OFF)) ==
+							FP_TG_CONTROL_OFF);
+}
+
 static void
 nv_output_prepare(xf86OutputPtr output)
 {
@@ -963,8 +969,7 @@ static void dpms_update_fp_control(ScrnInfoPtr pScrn, struct nouveau_encoder *nv
 		nv_crtc = to_nouveau_crtc(crtc);
 		fpc = &pNv->ModeReg.crtc_reg[nv_crtc->head].fp_control;
 
-		if ((*fpc & (FP_TG_CONTROL_ON | FP_TG_CONTROL_OFF)) ==
-							FP_TG_CONTROL_OFF)
+		if (is_fpc_off(*fpc))
 			/* using saved value is ok, as (is_digital && dpms_on &&
 			 * fp_control==OFF) is (at present) *only* true when
 			 * fpc's most recent change was by below "off" code
@@ -979,7 +984,7 @@ static void dpms_update_fp_control(ScrnInfoPtr pScrn, struct nouveau_encoder *nv
 			fpc = &pNv->ModeReg.crtc_reg[nv_crtc->head].fp_control;
 
 			nv_crtc->fp_users &= ~(1 << nv_encoder->dcb->index);
-			if (!nv_crtc->fp_users) {
+			if (!is_fpc_off(*fpc) && !nv_crtc->fp_users) {
 				nv_crtc->dpms_saved_fp_control = *fpc;
 				/* cut the FP output */
 				*fpc &= ~FP_TG_CONTROL_ON;
@@ -990,10 +995,17 @@ static void dpms_update_fp_control(ScrnInfoPtr pScrn, struct nouveau_encoder *nv
 		}
 }
 
+static bool is_powersaving_dpms(int mode)
+{
+	return (mode == DPMSModeStandby || mode == DPMSModeSuspend ||
+							mode == DPMSModeOff);
+}
+
 static void
 lvds_encoder_dpms(ScrnInfoPtr pScrn, struct nouveau_encoder *nv_encoder, xf86CrtcPtr crtc, int mode)
 {
 	NVPtr pNv = NVPTR(pScrn);
+	bool was_powersaving = is_powersaving_dpms(nv_encoder->last_dpms);
 
 	if (nv_encoder->last_dpms == mode)
 		return;
@@ -1001,6 +1013,9 @@ lvds_encoder_dpms(ScrnInfoPtr pScrn, struct nouveau_encoder *nv_encoder, xf86Crt
 
 	NV_TRACE(pScrn, "Setting dpms mode %d on lvds encoder (output %d)\n",
 		 mode, nv_encoder->dcb->index);
+
+	if (was_powersaving && is_powersaving_dpms(mode))
+		return;
 
 	if (nv_encoder->dcb->lvdsconf.use_power_scripts) {
 		/* when removing an output, crtc may not be set, but PANEL_OFF