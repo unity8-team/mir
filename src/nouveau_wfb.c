ccdaa84cd17372fae9d2c79ff69f85cb5a64a154
Ben Skeggs <bskeggs@redhat.com>
2009-06-10 11:33:03 +1000

    wfb: work-around wfb suckage
    wfb sucks far more than anticipated:
     - can only expose a single set of rd/wr hooks, not per-wrap as wfb doesn't
       treat the hooks as per-pixmap.
     - the hooks will get called on buffers that aren't even pixmaps

--- src/nouveau_wfb.c
+++ src/nouveau_wfb.c
@@ -28,9 +28,12 @@
 #include "nv_include.h"
 
 struct wfb_pixmap {
+	PixmapPtr ppix;
 	unsigned long base;
 	unsigned long end;
 	unsigned pitch;
+	unsigned tile_height;
+	unsigned tile_pitch;
 	unsigned horiz_tiles;
 	uint64_t multiply_factor;
 };
@@ -52,123 +55,72 @@ nouveau_wfb_wr_linear(void *dst, FbBits value, int size)
 	memcpy(dst, &value, size);
 }
 
-#define TILE_FUNC_FOR_WRAP(name,wrap)                                          \
-static FbBits                                                                  \
-nouveau_wfb_rd_##name(const void *ptr, int size) {                             \
-	unsigned long offset = (unsigned long)ptr;                             \
-	struct wfb_pixmap *wfb = &wfb_pixmap[wrap];                            \
-	FbBits bits = 0;                                                       \
-	int x, y;                                                              \
-                                                                               \
-	offset -= wfb->base;                                                   \
-                                                                               \
-	y = (offset * wfb->multiply_factor) >> 32;                             \
-	x = offset - y * wfb->pitch;                                           \
-                                                                               \
-	offset  = (x >> TP) + ((y >> TH) * wfb->horiz_tiles);                  \
-	offset *= (1 << (TH + TP));                                            \
-	offset += ((y & THM) * (1 << TP)) + (x & TPM);                         \
-                                                                               \
-	memcpy(&bits, (void *)wfb->base + offset, size);                       \
-	return bits;                                                           \
-}                                                                              \
-                                                                               \
-static void                                                                    \
-nouveau_wfb_wr_##name(void *ptr, FbBits value, int size) {                     \
-	unsigned long offset = (unsigned long)ptr;                             \
-	struct wfb_pixmap *wfb = &wfb_pixmap[wrap];                            \
-	int x, y;                                                              \
-                                                                               \
-	offset -= wfb->base;                                                   \
-                                                                               \
-	y = (offset * wfb->multiply_factor) >> 32;                             \
-	x = offset - y * wfb->pitch;                                           \
-                                                                               \
-	offset  = (x >> TP) + ((y >> TH) * wfb->horiz_tiles);                  \
-	offset *= (1 << (TH + TP));                                            \
-	offset += ((y & THM) * (1 << TP)) + (x & TPM);                         \
-                                                                               \
-	memcpy((void *)wfb->base + offset, &value, size);                      \
-}
+#define TP wfb->tile_pitch
+#define TH wfb->tile_height
+#define TPM ((1 << wfb->tile_pitch) - 1)
+#define THM ((1 << wfb->tile_height) - 1)
 
-#define TILE_FUNC(name)                                                        \
-	TILE_FUNC_FOR_WRAP(w00_##name, 0);                                     \
-	TILE_FUNC_FOR_WRAP(w01_##name, 1);                                     \
-	TILE_FUNC_FOR_WRAP(w02_##name, 2);                                     \
-	TILE_FUNC_FOR_WRAP(w03_##name, 3);                                     \
-	TILE_FUNC_FOR_WRAP(w04_##name, 4);                                     \
-	TILE_FUNC_FOR_WRAP(w05_##name, 5);                                     \
-	static struct {                                                        \
-		ReadMemoryProcPtr read;                                        \
-		WriteMemoryProcPtr write;                                      \
-	} nouveau_wfb_##name[6] = {                                            \
-		{ nouveau_wfb_rd_w00_##name, nouveau_wfb_wr_w00_##name },      \
-		{ nouveau_wfb_rd_w01_##name, nouveau_wfb_wr_w01_##name },      \
-		{ nouveau_wfb_rd_w02_##name, nouveau_wfb_wr_w02_##name },      \
-		{ nouveau_wfb_rd_w03_##name, nouveau_wfb_wr_w03_##name },      \
-		{ nouveau_wfb_rd_w04_##name, nouveau_wfb_wr_w04_##name },      \
-		{ nouveau_wfb_rd_w05_##name, nouveau_wfb_wr_w05_##name },      \
+static FbBits
+nouveau_wfb_rd_tiled(const void *ptr, int size) {
+	unsigned long offset = (unsigned long)ptr;
+	struct wfb_pixmap *wfb = NULL;
+	FbBits bits = 0;
+	int x, y, i;
+
+	for (i = 0; i < 6; i++) {
+		if (offset >= wfb_pixmap[i].base &&
+		    offset < wfb_pixmap[i].end) {
+			wfb = &wfb_pixmap[i];
+			break;
+		}
 	}
 
-#define HW_MODE 0
-#define TH (HW_MODE + 2)
-#define TP 5
-#define THM ((1 << TH) - 1)
-#define TPM ((1 << TP) - 1)
-TILE_FUNC(mode0);
-#undef TPM
-#undef THM
-#undef TP
-#undef TH
-#undef HW_MODE
+	if (!wfb || !wfb->tile_pitch)
+		return nouveau_wfb_rd_linear(ptr, size);
 
-#define HW_MODE 1
-#define TH (HW_MODE + 2)
-#define TP 6
-#define THM ((1 << TH) - 1)
-#define TPM ((1 << TP) - 1)
-TILE_FUNC(mode1);
-#undef TPM
-#undef THM
-#undef TP
-#undef TH
-#undef HW_MODE
+	offset -= wfb->base;
 
-#define HW_MODE 2
-#define TH (HW_MODE + 2)
-#define TP 6
-#define THM ((1 << TH) - 1)
-#define TPM ((1 << TP) - 1)
-TILE_FUNC(mode2);
-#undef TPM
-#undef THM
-#undef TP
-#undef TH
-#undef HW_MODE
+	y = (offset * wfb->multiply_factor) >> 32;
+	x = offset - y * wfb->pitch;
 
-#define HW_MODE 3
-#define TH (HW_MODE + 2)
-#define TP 6
-#define THM ((1 << TH) - 1)
-#define TPM ((1 << TP) - 1)
-TILE_FUNC(mode3);
-#undef TPM
-#undef THM
-#undef TP
-#undef TH
-#undef HW_MODE
+	offset  = (x >> TP) + ((y >> TH) * wfb->horiz_tiles);
+	offset *= (1 << (TH + TP));
+	offset += ((y & THM) * (1 << TP)) + (x & TPM);
 
-#define HW_MODE 4
-#define TH (HW_MODE + 2)
-#define TP 6
-#define THM ((1 << TH) - 1)
-#define TPM ((1 << TP) - 1)
-TILE_FUNC(mode4);
-#undef TPM
-#undef THM
-#undef TP
-#undef TH
-#undef HW_MODE
+	memcpy(&bits, (void *)wfb->base + offset, size);
+	return bits;
+}
+
+static void
+nouveau_wfb_wr_tiled(void *ptr, FbBits value, int size) {
+	unsigned long offset = (unsigned long)ptr;
+	struct wfb_pixmap *wfb = NULL;
+	int x, y, i;
+
+	for (i = 0; i < 6; i++) {
+		if (offset >= wfb_pixmap[i].base &&
+		    offset < wfb_pixmap[i].end) {
+			wfb = &wfb_pixmap[i];
+			break;
+		}
+	}
+
+	if (!wfb || !wfb->tile_pitch) {
+		nouveau_wfb_wr_linear(ptr, value, size);
+		return;
+	}
+
+	offset -= wfb->base;
+
+	y = (offset * wfb->multiply_factor) >> 32;
+	x = offset - y * wfb->pitch;
+
+	offset  = (x >> TP) + ((y >> TH) * wfb->horiz_tiles);
+	offset *= (1 << (TH + TP));
+	offset += ((y & THM) * (1 << TP)) + (x & TPM);
+
+	memcpy((void *)wfb->base + offset, &value, size);
+}
 
 void
 nouveau_wfb_setup_wrap(ReadMemoryProcPtr *pRead, WriteMemoryProcPtr *pWrite,
@@ -188,46 +140,25 @@ nouveau_wfb_setup_wrap(ReadMemoryProcPtr *pRead, WriteMemoryProcPtr *pWrite,
 	nvpix = nouveau_pixmap(ppix);
 
 	wrap = 0;
-	while (wfb_pixmap[wrap].base)
+	while (wfb_pixmap[wrap].ppix)
 		wrap++;
 	wfb = &wfb_pixmap[wrap];
 
+	wfb->ppix = ppix;
 	wfb->base = (unsigned long)ppix->devPrivate.ptr;
 	wfb->end = wfb->base + nvpix->bo->size;
-	wfb->pitch = ppix->devKind;
-	wfb->multiply_factor = (0xFFFFFFFF / wfb->pitch) + 1;
-	wfb->horiz_tiles = wfb->pitch / (nvpix->bo->tile_mode ? 64 : 32);
-
 	if (!nvpix->bo->tile_flags) {
-		*pRead = nouveau_wfb_rd_linear;
-		*pWrite = nouveau_wfb_wr_linear;
-		return;
+		wfb->tile_pitch = 0;
+	} else {
+		wfb->pitch = ppix->devKind;
+		wfb->multiply_factor = (0xFFFFFFFF / wfb->pitch) + 1;
+		wfb->tile_height = nvpix->bo->tile_mode + 2;
+		wfb->tile_pitch = nvpix->bo->tile_mode ? 6 : 5;
+		wfb->horiz_tiles = wfb->pitch / (1 << wfb->tile_pitch);
 	}
 
-	switch (nvpix->bo->tile_mode) {
-	case 0:
-		*pRead = nouveau_wfb_mode0[wrap].read;
-		*pWrite = nouveau_wfb_mode0[wrap].write;
-		break;
-	case 1:
-		*pRead = nouveau_wfb_mode1[wrap].read;
-		*pWrite = nouveau_wfb_mode1[wrap].write;
-		break;
-	case 2:
-		*pRead = nouveau_wfb_mode2[wrap].read;
-		*pWrite = nouveau_wfb_mode2[wrap].write;
-		break;
-	case 3:
-		*pRead = nouveau_wfb_mode3[wrap].read;
-		*pWrite = nouveau_wfb_mode3[wrap].write;
-		break;
-	case 4:
-		*pRead = nouveau_wfb_mode4[wrap].read;
-		*pWrite = nouveau_wfb_mode4[wrap].write;
-		break;
-	default:
-		FatalError("uh oh\n");
-	}
+	*pRead = nouveau_wfb_rd_tiled;
+	*pWrite = nouveau_wfb_wr_tiled;
 }
 
 void
@@ -240,8 +171,8 @@ nouveau_wfb_finish_wrap(DrawablePtr pDraw)
 	if (!ppix)
 		return;
 
-	while (wfb->base != (unsigned long)ppix->devPrivate.ptr)
+	while (wfb->ppix != ppix)
 		wfb++;
-	wfb->base = 0;
+	wfb->ppix = NULL;
 }
 
