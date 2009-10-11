602ed25fb69e53ac5eaf2168a55815f67f6f01d7
Francisco Jerez <currojerez@riseup.net>
2009-10-11 23:54:51 +0200

    Partially revert "kms: always set gamma on modeset"
    6c8d3e39b72abe897b08d9e90cae85e121863d2c breaks gamma setting with
    servers older than 1.7. Keep the call to xf86HandleColormaps even with
    KMS enabled, as it's a no-op in the cases we don't need it.
    
    Signed-off-by: Francisco Jerez <currojerez@riseup.net>

--- src/nv_driver.c
+++ src/nv_driver.c
@@ -1690,8 +1690,7 @@ NVScreenInit(int scrnIndex, ScreenPtr pScreen, int argc, char **argv)
 	 * Initialize colormap layer.
 	 * Must follow initialization of the default colormap 
 	 */
-	if (!pNv->kms_enable &&
-	    !xf86HandleColormaps(pScreen, 256, 8, NVLoadPalette,
+	if (!xf86HandleColormaps(pScreen, 256, 8, NVLoadPalette,
 				 NULL, CMAP_PALETTED_TRUECOLOR))
 		return FALSE;
 
