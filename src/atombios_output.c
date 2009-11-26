558db7b4810fd2ddcc9c9e2e44074c4b5175e733
Dave Airlie <airlied@redhat.com>
2009-11-26 15:43:54 +1000

    dce3: no CV1 or TV1 Output Control tables anymore.
    According to the DCE3 docs we should only use DAC1/2 not
    CV1/TV OutputControl, also my rv730 bios doesn't have any
    CV1/TV tables.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/atombios_output.c
+++ src/atombios_output.c
@@ -1256,19 +1256,26 @@ atombios_output_dpms(xf86OutputPtr output, int mode)
 	break;
     case ENCODER_OBJECT_ID_INTERNAL_DAC1:
     case ENCODER_OBJECT_ID_INTERNAL_KLDSCP_DAC1:
-	if (radeon_output->active_device & (ATOM_DEVICE_TV_SUPPORT))
-	    index = GetIndexIntoMasterTable(COMMAND, TV1OutputControl);
-	else if (radeon_output->active_device & (ATOM_DEVICE_CV_SUPPORT))
-	    index = GetIndexIntoMasterTable(COMMAND, CV1OutputControl);
-	else
+	if (!IS_DCE3_VARIANT) {
+	    if (radeon_output->active_device & (ATOM_DEVICE_TV_SUPPORT))
+		index = GetIndexIntoMasterTable(COMMAND, TV1OutputControl);
+	    else if (radeon_output->active_device & (ATOM_DEVICE_CV_SUPPORT))
+		index = GetIndexIntoMasterTable(COMMAND, CV1OutputControl);
+	    else
+		index = GetIndexIntoMasterTable(COMMAND, DAC1OutputControl);
+	}  else
 	    index = GetIndexIntoMasterTable(COMMAND, DAC1OutputControl);
 	break;
     case ENCODER_OBJECT_ID_INTERNAL_DAC2:
     case ENCODER_OBJECT_ID_INTERNAL_KLDSCP_DAC2:
-	if (radeon_output->active_device & (ATOM_DEVICE_TV_SUPPORT))
-	    index = GetIndexIntoMasterTable(COMMAND, TV1OutputControl);
-	else if (radeon_output->active_device & (ATOM_DEVICE_CV_SUPPORT))
-	    index = GetIndexIntoMasterTable(COMMAND, CV1OutputControl);
+	if (!IS_DCE3_VARIANT) {
+	    if (radeon_output->active_device & (ATOM_DEVICE_TV_SUPPORT))
+		index = GetIndexIntoMasterTable(COMMAND, TV1OutputControl);
+	    else if (radeon_output->active_device & (ATOM_DEVICE_CV_SUPPORT))
+		index = GetIndexIntoMasterTable(COMMAND, CV1OutputControl);
+	    else
+		index = GetIndexIntoMasterTable(COMMAND, DAC2OutputControl);
+	}
 	else
 	    index = GetIndexIntoMasterTable(COMMAND, DAC2OutputControl);
 	break;
