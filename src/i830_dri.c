d043924977f2100bed8358043dda7a55d174a65b
Jesse Barnes <jbarnes@virtuousgeek.org>
2010-02-05 17:18:50 -0800

    DRI2: only use version 4 APIs if kernel support exists
    Check for page flipping support before enabling flip and vblank event
    support needed for the new DRI2 APIs.
    
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- src/i830_dri.c
+++ src/i830_dri.c
@@ -907,13 +907,15 @@ Bool I830DRI2ScreenInit(ScreenPtr screen)
 
 	info.CopyRegion = I830DRI2CopyRegion;
 #if DRI2INFOREC_VERSION >= 4
-	info.version = 4;
-	info.ScheduleSwap = I830DRI2ScheduleSwap;
-	info.GetMSC = I830DRI2GetMSC;
-	info.ScheduleWaitMSC = I830DRI2ScheduleWaitMSC;
-	info.numDrivers = 1;
-	info.driverNames = driverNames;
-	driverNames[0] = info.driverName;
+	if (intel->use_pageflipping) {
+	    info.version = 4;
+	    info.ScheduleSwap = I830DRI2ScheduleSwap;
+	    info.GetMSC = I830DRI2GetMSC;
+	    info.ScheduleWaitMSC = I830DRI2ScheduleWaitMSC;
+	    info.numDrivers = 1;
+	    info.driverNames = driverNames;
+	    driverNames[0] = info.driverName;
+	}
 #endif
 
 	return DRI2ScreenInit(screen, &info);
