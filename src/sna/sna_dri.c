2980aaca062eb1a8e4f82f567c7c0a6bd8929b53
Adam Jackson <ajax@redhat.com>
2011-06-22 22:39:37 +0100

    Don't include xf86Priv.h
    I'd like it to not be in the SDK anymore, and we're not using anything
    from it.
    
    Signed-off-by: Adam Jackson <ajax@redhat.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/sna/sna_dri.c
+++ src/sna/sna_dri.c
@@ -51,7 +51,6 @@ USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "xf86.h"
 #include "xf86_OSproc.h"
-#include "xf86Priv.h"
 
 #include "xf86PciInfo.h"
 #include "xf86Pci.h"
--- src/sna/sna_driver.c
+++ src/sna/sna_driver.c
@@ -47,7 +47,6 @@ USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "xf86.h"
 #include "xf86_OSproc.h"
-#include "xf86Priv.h"
 #include "xf86cmap.h"
 #include "compiler.h"
 #include "mibstore.h"
