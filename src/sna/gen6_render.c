408abf4379fa48dab987a04c3c7a277e1c5c0f03
Chris Wilson <chris@chris-wilson.co.uk>
2011-09-12 11:54:46 +0100

    sna/gen6: Prefer RENDER for copies as it compacts better
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/sna/gen6_render.c
+++ src/sna/gen6_render.c
@@ -2307,8 +2307,7 @@ gen6_render_copy_boxes(struct sna *sna, uint8_t alu,
 	     __FUNCTION__, src_dx, src_dy, dst_dx, dst_dy, n, alu,
 	     src_bo == dst_bo));
 
-	/* XXX benchmark me! */
-	if (sna->kgem.mode != KGEM_RENDER &&
+	if (sna->kgem.mode == KGEM_BLT &&
 	    sna_blt_compare_depth(&src->drawable, &dst->drawable) &&
 	    sna_blt_copy_boxes(sna, alu,
 			       src_bo, src_dx, src_dy,
@@ -2465,8 +2464,7 @@ gen6_render_copy(struct sna *sna, uint8_t alu,
 	     src->drawable.width, src->drawable.height,
 	     dst->drawable.width, dst->drawable.height));
 
-	/* XXX benchmark me! */
-	if (sna->kgem.mode != KGEM_RENDER &&
+	if (sna->kgem.mode == KGEM_BLT &&
 	    sna_blt_compare_depth(&src->drawable, &dst->drawable) &&
 	    sna_blt_copy(sna, alu,
 			 src_bo, dst_bo,
