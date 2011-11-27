9d5c54fff8f196b386750a40403e3948fd55471b
Chris Wilson <chris@chris-wilson.co.uk>
2011-11-27 09:04:10 +0000

    sna: Disabling tiling if the bo would be smaller than a page
    Once again experiment with untiled smalled buffers.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/sna/kgem.c
+++ src/sna/kgem.c
@@ -1434,6 +1434,16 @@ int kgem_choose_tiling(struct kgem *kgem, int tiling, int width, int height, int
 		}
 	}
 
+	if (tiling &&
+	    ALIGN(height, 2) * ALIGN(width*bpp, 8*64) < 4096 * 8) {
+		DBG(("%s: too small [%d] for TILING_%c\n",
+		     __FUNCTION__,
+		     ALIGN(height, 2) * ALIGN(width*bpp, 8*64) / 8,
+		     tiling == I915_TILING_X ? 'X' : 'Y'));
+		tiling = I915_TILING_NONE;
+		goto done;
+	}
+
 done:
 	DBG(("%s: %dx%d -> %d\n", __FUNCTION__, width, height, tiling));
 	return tiling;
