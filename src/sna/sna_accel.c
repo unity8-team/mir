8e37276c24a5ec6ef731595ffcfbe709398e7f46
Chris Wilson <chris@chris-wilson.co.uk>
2011-11-27 10:18:35 +0000

    sna: Eliminate empty glyphs during format conversion
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/sna/sna_accel.c
+++ src/sna/sna_accel.c
@@ -7371,6 +7371,12 @@ static bool sna_set_glyph(CharInfoPtr in, CharInfoPtr out)
 		return false;
 	}
 
+	/* Skip empty glyphs */
+	if (w == 1 && h == 1 && (in->bits[0] & 1) == 0) {
+		out->bits = (void *)-1;
+		return false;
+	}
+
 	w = (w + 7) >> 3;
 
 	out->metrics = in->metrics;
