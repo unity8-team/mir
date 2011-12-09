9c2c97809324b1a66ffa2cd9e11f58cf4c5f9247
Chris Wilson <chris@chris-wilson.co.uk>
2011-12-09 17:25:19 +0000

    sna: s/MONOTONICE/MONOTONIC/
    A late addition to be flexible for compiling on different systems
    heralded its doom.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/sna/sna_accel.c
+++ src/sna/sna_accel.c
@@ -8513,7 +8513,7 @@ static void sna_accel_create_timers(struct sna *sna)
 {
 	int id;
 
-#ifdef CLOCK_MONOTONICE_COARSE
+#ifdef CLOCK_MONOTONIC_COARSE
 	for (id = 0; id < NUM_FINE_TIMERS; id++)
 		sna->timer[id] = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
 	for (; id < NUM_TIMERS; id++)
