7009337472f856492de021fcd1cca25fd7cbb1aa
Michel Dänzer <michel.daenzer@amd.com>
2012-04-16 15:52:59 +0200

    RADEONCopySwap: Fix RADEON_HOST_DATA_SWAP_16BIT case.
    It was the same code as for RADEON_HOST_DATA_SWAP_32BIT. This caused bus errors
    on FreeBSD/PPC, but I'm not sure how it could not cause problems anywhere...
    
    Reported-by: Andreas Tobler <andreast@fgznet.ch>
    Signed-off-by: Michel Dänzer <michel.daenzer@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- src/radeon_accel.c
+++ src/radeon_accel.c
@@ -982,10 +982,9 @@ void RADEONCopySwap(uint8_t *dst, uint8_t *src, unsigned int size, int swap)
 
 	    for (; nwords > 0; --nwords, ++d, ++s)
 #ifdef __powerpc__
-		asm volatile("stwbrx %0,0,%1" : : "r" (*s), "r" (d));
+		asm volatile("sthbrx %0,0,%1" : : "r" (*s), "r" (d));
 #else
-	        *d = ((*s >> 24) & 0xff) | ((*s >> 8) & 0xff00)
-			| ((*s & 0xff00) << 8) | ((*s & 0xff) << 24);
+	        *d = (*s >> 8) | (*s << 8);
 #endif
 	    return;
 	}
