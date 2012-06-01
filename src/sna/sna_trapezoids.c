b70df51c7222357bd26cab764cb9732f94ee21d8
Chris Wilson <chris@chris-wilson.co.uk>
2012-06-01 13:02:50 +0100

    sna/trapezoids: Correct extents declaration for fallback
    Reported-by: Zdenek Kabelac <zkabelac@redhat.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/sna/sna_trapezoids.c
+++ src/sna/sna_trapezoids.c
@@ -4428,7 +4428,7 @@ trapezoid_span_fallback(CARD8 op, PicturePtr src, PicturePtr dst,
 		region.extents.x1 = dst_x + dst->pDrawable->x;
 		region.extents.y1 = dst_y + dst->pDrawable->y;
 		region.extents.x2 = region.extents.x1 + extents.x2;
-		region.extents.y2 = region.extents.y2 + extents.y2;
+		region.extents.y2 = region.extents.y1 + extents.y2;
 		region.data = NULL;
 
 		DBG(("%s: move-to-cpu\n", __FUNCTION__));
