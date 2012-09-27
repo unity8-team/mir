7957882172e52301c8078dbaffee153fef232c21
Chris Wilson <chris@chris-wilson.co.uk>
2012-09-27 14:58:57 +0100

    Remove the last couple of references to mibstore
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/legacy/i810/i810_driver.c
+++ src/legacy/i810/i810_driver.c
@@ -58,7 +58,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "xf86cmap.h"
 
 #include "compiler.h"
-#include "mibstore.h"
 #include "vgaHW.h"
 #include "mipointer.h"
 #include "micmap.h"
@@ -1675,7 +1674,6 @@ I810ScreenInit(SCREEN_INIT_ARGS_DECL)
       I810EmitFlush(scrn);
    }
 
-   miInitializeBackingStore(screen);
    xf86SetBackingStore(screen);
    xf86SetSilkenMouse(screen);
 
