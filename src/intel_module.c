2c118078c28e72ca634a0533c85424d46014b23f
Chris Wilson <chris@chris-wilson.co.uk>
2013-02-05 14:45:39 +0000

    intel: add more ValleyView PCI IDs
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/intel_module.c
+++ src/intel_module.c
@@ -306,6 +306,9 @@ static const struct pci_id_match intel_device_match[] = {
 	INTEL_DEVICE_MATCH (PCI_CHIP_HASWELL_CRW_S_GT2_PLUS, &intel_haswell_info ),
 
 	INTEL_DEVICE_MATCH (PCI_CHIP_VALLEYVIEW_PO, &intel_valleyview_info ),
+	INTEL_DEVICE_MATCH (PCI_CHIP_VALLEYVIEW_1, &intel_valleyview_info ),
+	INTEL_DEVICE_MATCH (PCI_CHIP_VALLEYVIEW_2, &intel_valleyview_info ),
+	INTEL_DEVICE_MATCH (PCI_CHIP_VALLEYVIEW_3, &intel_valleyview_info ),
 
 	INTEL_DEVICE_MATCH (PCI_MATCH_ANY, &intel_generic_info ),
 #endif
