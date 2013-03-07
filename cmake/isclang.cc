71b84aef9141a27e97ab9f4425c377b77a8f32b2
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-03-07 16:24:56 +0200

    Add precompiled header support and enable it in compositor.

---
+++ cmake/isclang.cc
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2012 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU Lesser General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+int main(int argc, char **argv) {
+#ifdef __clang__
+    return 1; // This gets assigned to a CMake variable so "1" means "true".
+#else
+    return 0;
+#endif
+}
