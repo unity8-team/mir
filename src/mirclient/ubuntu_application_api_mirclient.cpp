b71ad4fde7b8393b51afe9d91b992d92dd9a3e4b
Robert Carr <robert.carr@canonical.com>
2013-04-01 16:26:39 -0700

    Skeleton implementation to make qtubuntu work

--- src/mirclient/ubuntu_application_api_mirclient.cpp
+++ src/mirclient/ubuntu_application_api_mirclient.cpp
@@ -17,6 +17,15 @@
  */
 
 #include <ubuntu/application/ui/ubuntu_application_ui.h>
+#include <mir_toolkit/mir_client_library.h>
+
+#include <assert.h>
+
+namespace
+{
+    char const* const socket_file = "/tmp/mir_socket";
+    MirConnection *global_connection;
+}
     
 void
 ubuntu_application_ui_init(int argc, char**argv)
@@ -40,13 +49,15 @@ ubuntu_application_ui_setup_get_form_factor_hint()
 void
 ubuntu_application_ui_start_a_new_session(SessionCredentials* creds)
 {
-    // TODO: Implement ~racarr
+    global_connection = mir_connect_sync(socket_file, creds->application_name);
+    assert(global_connection);
 }
 
 EGLNativeDisplayType
 ubuntu_application_ui_get_native_display()
 {
-    return (EGLNativeDisplayType)0;
+    assert(global_connection);
+    return (EGLNativeDisplayType)mir_connection_get_egl_native_display(global_connection);
 }
 
 void
@@ -58,6 +69,8 @@ ubuntu_application_ui_set_clipboard_content(void* data, size_t size)
 void
 ubuntu_application_ui_get_clipboard_content(void** data, size_t* size)
 {
+    *size = 0;
+    *data = NULL;
     // TODO: Implement ~racarr
 }
 
@@ -65,26 +78,36 @@ void
 ubuntu_application_ui_create_display_info(ubuntu_application_ui_physical_display_info* info, size_t index)
 {
     // TODO: Implement ~racarr
+    // Noop for mirclient?
 }
 
 void
 ubuntu_application_ui_destroy_display_info(ubuntu_application_ui_physical_display_info info)
 {
     // TODO: Implement ~racarr
+    // Noop for mirclient?
 }
 
 int32_t
 ubuntu_application_ui_query_horizontal_resolution(ubuntu_application_ui_physical_display_info info)
 {
-    // TODO: Implement ~racarr
-    return (int32_t) 0;
+    MirDisplayInfo display_info;
+
+    assert(global_connection);
+
+    mir_connection_get_display_info(global_connection, &display_info);
+    return (int32_t)display_info.width;
 }
 
 int32_t
 ubuntu_application_ui_query_vertical_resolution(ubuntu_application_ui_physical_display_info info)
 {
-    // TODO: Implement ~racarr
-    return (int32_t) 0;
+    MirDisplayInfo display_info;
+    
+    assert(global_connection);
+
+    mir_connection_get_display_info(global_connection, &display_info);
+    return (int32_t)display_info.height;
 }
 
 float
@@ -101,6 +124,21 @@ ubuntu_application_ui_query_vertical_dpi(ubuntu_application_ui_physical_display_
     return (float)0.0;
 }
 
+namespace
+{
+struct EventContext
+{
+    input_event_cb cb;
+    void* ctx;
+};
+
+static void ubuntu_application_ui_mir_handle_event(MirSurface* surface, MirEvent* ev, void* context)
+{
+    EventContext *evctx = static_cast<EventContext*>(context);
+    evctx->cb(evctx->ctx, (Event*)ev); // TODO: Proper event translation ~racarr
+}
+}
+
 void
 ubuntu_application_ui_create_surface(ubuntu_application_ui_surface* out_surface,
                                      const char* title,
@@ -111,7 +149,18 @@ ubuntu_application_ui_create_surface(ubuntu_application_ui_surface* out_surface,
                                      input_event_cb cb,
                                      void* ctx)
 {
+    assert(global_connection);
+    MirDisplayInfo display_info;
+    mir_connection_get_display_info(global_connection, &display_info);
+    MirSurfaceParameters params = { title, width, height, display_info.supported_pixel_format[0], mir_buffer_usage_hardware};
+    EventContext *delegate_ctx = new EventContext; // TODO: Fix leak ~racarr
+    delegate_ctx->cb = cb;
+    delegate_ctx->ctx = ctx;
+    MirEventDelegate event_delegate = { ubuntu_application_ui_mir_handle_event, delegate_ctx };
+    MirSurface* surface = mir_surface_create_sync(global_connection, &params, &event_delegate);
+    assert(surface);
     // TODO: Implement ~racarr
+    *out_surface = static_cast<ubuntu_application_ui_surface>(surface);
 }
 
 void
@@ -123,14 +172,14 @@ ubuntu_application_ui_request_fullscreen_for_surface(ubuntu_application_ui_surfa
 void
 ubuntu_application_ui_destroy_surface(ubuntu_application_ui_surface surface)
 {
-    // TODO: Implement ~racarr
+    MirSurface *mir_surface = static_cast<MirSurface*>(surface);
+    mir_surface_release_sync(mir_surface);
 }
 
 EGLNativeWindowType
 ubuntu_application_ui_surface_to_native_window_type(ubuntu_application_ui_surface surface)
 {
-    // TODO: Implement ~racarr
-    return (EGLNativeWindowType)0;
+    return reinterpret_cast<EGLNativeWindowType>(mir_surface_get_egl_native_window(static_cast<MirSurface*>(surface)));
 }
 
 void
