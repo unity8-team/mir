1f080650fa0dc87e648bded340e97da7ddb3a6cf
Robert Carr <robert.carr@canonical.com>
2013-04-18 15:35:47 -0700

    Implement event translation

--- src/mirclient/ubuntu_application_api_mirclient.cpp
+++ src/mirclient/ubuntu_application_api_mirclient.cpp
@@ -136,10 +136,66 @@ struct EventContext
     void* ctx;
 };
 
-static void ubuntu_application_ui_mir_handle_event(MirSurface* surface, MirEvent* ev, void* context)
+static void
+mir_event_to_ubuntu_event(MirEvent const* mir_event, Event& ubuntu_ev)
+{
+    switch (mir_event->type)
+    {
+    case mir_event_type_key:
+        ubuntu_ev.type = KEY_EVENT_TYPE;
+        ubuntu_ev.device_id = mir_event->key.device_id;
+        ubuntu_ev.source_id = mir_event->key.source_id;
+        ubuntu_ev.action = mir_event->key.action;
+        ubuntu_ev.flags = mir_event->key.flags;
+        ubuntu_ev.meta_state = mir_event->key.meta_state;
+        ubuntu_ev.details.key.key_code = mir_event->key.key_code;
+        ubuntu_ev.details.key.scan_code = mir_event->key.scan_code;
+        ubuntu_ev.details.key.repeat_count = mir_event->key.repeat_count;
+        ubuntu_ev.details.key.down_time = mir_event->key.down_time;
+        ubuntu_ev.details.key.event_time = mir_event->key.event_time;
+        ubuntu_ev.details.key.is_system_key = mir_event->key.is_system_key;
+        break;
+    case mir_event_type_motion:
+        ubuntu_ev.type = MOTION_EVENT_TYPE;
+        ubuntu_ev.device_id = mir_event->motion.device_id;
+        ubuntu_ev.source_id = mir_event->motion.source_id;
+        ubuntu_ev.action = mir_event->motion.action;
+        ubuntu_ev.flags = mir_event->motion.flags;
+        ubuntu_ev.meta_state = mir_event->motion.meta_state;
+        ubuntu_ev.details.motion.edge_flags = mir_event->motion.edge_flags;
+        ubuntu_ev.details.motion.button_state = mir_event->motion.button_state;
+        ubuntu_ev.details.motion.x_offset = mir_event->motion.x_offset;
+        ubuntu_ev.details.motion.y_offset = mir_event->motion.y_offset;
+        ubuntu_ev.details.motion.x_precision = mir_event->motion.x_precision;
+        ubuntu_ev.details.motion.y_precision = mir_event->motion.y_precision;
+        ubuntu_ev.details.motion.down_time = mir_event->motion.down_time;
+        ubuntu_ev.details.motion.event_time = mir_event->motion.event_time;
+        ubuntu_ev.details.motion.pointer_count = mir_event->motion.pointer_count;
+        for (int i = 0; i < mir_event->motion.pointer_count; i++)
+        {
+            ubuntu_ev.details.motion.pointer_coordinates[i].id = mir_event->motion.pointer_coordinates[i].id;
+            ubuntu_ev.details.motion.pointer_coordinates[i].x = mir_event->motion.pointer_coordinates[i].x;
+            ubuntu_ev.details.motion.pointer_coordinates[i].raw_x = mir_event->motion.pointer_coordinates[i].raw_x;
+            ubuntu_ev.details.motion.pointer_coordinates[i].y = mir_event->motion.pointer_coordinates[i].y;
+            ubuntu_ev.details.motion.pointer_coordinates[i].raw_y = mir_event->motion.pointer_coordinates[i].raw_y;
+            ubuntu_ev.details.motion.pointer_coordinates[i].touch_major = mir_event->motion.pointer_coordinates[i].touch_major;
+            ubuntu_ev.details.motion.pointer_coordinates[i].touch_minor = mir_event->motion.pointer_coordinates[i].touch_minor;
+            ubuntu_ev.details.motion.pointer_coordinates[i].size = mir_event->motion.pointer_coordinates[i].size;
+            ubuntu_ev.details.motion.pointer_coordinates[i].pressure = mir_event->motion.pointer_coordinates[i].pressure;
+            ubuntu_ev.details.motion.pointer_coordinates[i].orientation = mir_event->motion.pointer_coordinates[i].orientation;
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+static void ubuntu_application_ui_mir_handle_event(MirSurface* surface, MirEvent const* mir_ev, void* context)
 {
     EventContext *evctx = static_cast<EventContext*>(context);
-    evctx->cb(evctx->ctx, (Event*)ev); // TODO: Proper event translation ~racarr
+    Event ubuntu_ev;
+    mir_event_to_ubuntu_event(mir_ev, ubuntu_ev);
+    evctx->cb(evctx->ctx, &ubuntu_ev); // TODO: Proper event translation ~racarr
 }
 }
 
@@ -161,7 +217,8 @@ ubuntu_application_ui_create_surface(ubuntu_application_ui_surface* out_surface,
     delegate_ctx->cb = cb;
     delegate_ctx->ctx = ctx;
     MirEventDelegate event_delegate = { ubuntu_application_ui_mir_handle_event, delegate_ctx };
-    MirSurface* surface = mir_surface_create_sync(global_connection, &params, &event_delegate);
+    MirSurface* surface = mir_connection_create_surface_sync(global_connection, &params);
+    mir_surface_set_event_handler(surface, &event_delegate);
     assert(surface);
     // TODO: Implement ~racarr
     *out_surface = static_cast<ubuntu_application_ui_surface>(surface);
