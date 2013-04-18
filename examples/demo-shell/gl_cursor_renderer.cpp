0ed1feb79ad2eea02e4f508180edd1ffaea8640d
Robert Carr <robert.carr@canonical.com>
2013-04-18 11:46:20 -0700

    Skeleton cursor renderer

---
+++ examples/demo-shell/gl_cursor_renderer.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Robert Carr <robert.carr@canonical.com>
+ */
+
+#include "gl_cursor_renderer.h"
+
+namespace me = mir::examples;
+
+me::GLCursorRenderer::GLCursorRenderer()
+{
+}
+
+void me::GLCursorRenderer::render_cursor(int x, int y)
+{
+    // TODO: Implement ~racarr
+    (void) x;
+    (void) y;
+}
---
+++ examples/demo-shell/gl_cursor_renderer.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Robert Carr <robert.carr@canonical.com>
+ */
+
+#ifndef MIR_EXAMPLES_GL_CURSOR_RENDERER_H_
+#define MIR_EXAMPLES_GL_CURSOR_RENDERER_H_
+
+namespace mir
+{
+namespace examples
+{
+
+class GLCursorRenderer
+{
+public:
+    GLCursorRenderer();
+    ~GLCursorRenderer() = default;
+    
+    void render_cursor(int x, int y);
+
+protected:
+    GLCursorRenderer(GLCursorRenderer const&) = delete;
+    GLCursorRenderer& operator=(GLCursorRenderer const&) = delete;
+
+private:
+    static int const cursor_width_px = 12;
+    static int const cursor_height_px = 12;
+};
+
+}
+} // namespace mir
+
+#endif // MIR_EXAMPLES_GL_CURSOR_RENDERER_H_
