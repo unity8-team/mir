4cb7370552b80079665e4add15ef1ee94cfe2808
Ricardo Mendoza <ricardo.mendoza@canonical.com>
2013-05-22 08:56:50 -0430

    Add copyright notices to headers and fix some inclusions

--- include/ubuntu/application/ui/stage.h
+++ include/ubuntu/application/ui/stage.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Ricardo Mendoza <ricardo.mendoza@canonical.com>
+ *              Thomas Voß <thomas.voss@canonical.com>           
+ */
+
 #ifndef UBUNTU_APPLICATION_UI_STAGE_H_
 #define UBUNTU_APPLICATION_UI_STAGE_H_
 
@@ -10,31 +29,5 @@ typedef enum
     U_SIDE_STAGE = 4,
     U_CONFIGURATION_STAGE = 5
 } UAUiStage;
-
-#ifndef UBUNTU_APPLICATION_UI_STAGE_HINT_H_
-#define UBUNTU_APPLICATION_UI_STAGE_HINT_H_
-
-/*namespace ubuntu
-{
-namespace application
-{
-namespace ui
-{
-enum StageHint
-{
-    main_stage = MAIN_STAGE_HINT,
-    integration_stage = INTEGRATION_STAGE_HINT,
-    share_stage = SHARE_STAGE_HINT,
-    content_picking_stage = CONTENT_PICKING_STAGE_HINT,
-    side_stage = SIDE_STAGE_HINT,
-    configuration_stage = CONFIGURATION_STAGE_HINT
-};  
-}
-}   
-} */  
-
-#endif /* UBUNTU_APPLICATION_UI_STAGE_HINT_H_ */
-
-//typedef ubuntu::application::ui::StageHint UApplicationUiStage;
 
 #endif /* UBUNTU_APPLICATION_UI_STAGE_H_ */
