f6ac6b0051bc5526ac64daa51f7c3ceb188c380a
Robert Carr <robert.carr@canonical.com>
2013-06-03 13:54:26 -0700

    Skeleton impl

---
+++ src/mirserver/ubuntu_application_api_mirserver.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2013 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Robert Carr <robert.carr@canonical.com>
+ */
+
+// C APIs
+#include <ubuntu/application/lifecycle_delegate.h>
+#include <ubuntu/application/ui/window.h>
+#include <ubuntu/application/ui/options.h>
+#include <ubuntu/application/ui/session.h>
+#include <ubuntu/application/ui/clipboard.h>
+#include <ubuntu/application/ui/display.h>
+#include <ubuntu/application/sensors/accelerometer.h>
+#include <ubuntu/application/sensors/proximity.h>
+#include <ubuntu/application/sensors/light.h>
+
+#include <mir/default_server_configuration.h>
+
+void compile() {}
