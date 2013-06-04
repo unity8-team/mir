3a795deaabda691cb61ed6351683cb4eb6de05f2
Robert Carr <robert.carr@canonical.com>
2013-06-04 16:24:29 -0700

    Application instance management functions on mirserver

--- src/mirserver/ubuntu_application_api_mirserver.cpp
+++ src/mirserver/ubuntu_application_api_mirserver.cpp
@@ -88,8 +88,15 @@ void ua_ui_mirserver_finish()
 // Application instance
 struct MirServerApplicationInstance
 {
+    MirServerApplicationInstance()
+        : ref_count(1)
+    {
+    }
+
     std::shared_ptr<mir::frontend::Session> session;
+    int ref_count;
 };
+
 MirServerApplicationInstance *
 u_application_mirserver_application(UApplicationInstance *instance)
 {
@@ -155,13 +162,42 @@ UApplicationInstance* u_application_instance_new_from_description_with_options(U
     assert(shell);
 
     // TODO: Make use of descriptions and options
-    // TODO: When do we free this?
     auto instance = new MirServerApplicationInstance;
 
     instance->session = shell->open_session(std::string("TODO: Name"),
         std::shared_ptr<mir::events::EventSink>());
 
     return mirserver_application_u_application(instance);
+}
+
+void
+u_application_instance_ref(UApplicationInstance *instance)
+{
+    auto mir_instance = u_application_mirserver_application(instance);
+    mir_instance->ref_count++;
+}
+    
+void
+u_application_instance_unref(UApplicationInstance *instance)
+{
+    auto mir_instance = u_application_mirserver_application(instance);
+    mir_instance->ref_count--;
+    if (mir_instance->ref_count == 0)
+        delete mir_instance;
+}
+    
+void
+u_application_instance_destroy(UApplicationInstance *instance)
+{
+    // TODO: What are the proper semantics here.
+    u_application_instance_unref(instance);
+}
+    
+void
+u_application_instance_run(UApplicationInstance *instance)
+{
+    // TODO: What is this supposed to do? Seems to be no-op on hybris.
+    (void) instance;
 }
 
 void ua_ui_set_clipboard_content(void* content, size_t content_size)
