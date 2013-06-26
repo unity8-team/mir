2d2f7eef1ed1419c6b7e392281b976742533626a
Kevin DuBois <kevin.dubois@canonical.com>
2013-06-26 14:56:11 -0700

    test to pass

--- src/server/frontend/session_mediator.cpp
+++ src/server/frontend/session_mediator.cpp
@@ -180,8 +180,7 @@ void mf::SessionMediator::next_buffer(
 
 void mf::SessionMediator::release_surface(
     google::protobuf::RpcController* /*controller*/,
-    //const mir::protobuf::SurfaceId* request,
-    const mir::protobuf::SurfaceId* /*request*/,
+    const mir::protobuf::SurfaceId* request,
     mir::protobuf::Void*,
     google::protobuf::Closure* done)
 {
@@ -190,9 +189,9 @@ void mf::SessionMediator::release_surface(
 
     report->session_release_surface_called(session->name());
 
-    //auto const id = SurfaceId(request->value());
+    auto const id = SurfaceId(request->value());
 
-//    session->destroy_surface(id);
+    shell->destroy_surface_for(session, id);
 
     done->Run();
 }
