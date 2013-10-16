678a8f764f533c26f336378e2f621c9b3f9b710b
Alan Griffiths <alan@octopull.co.uk>
2013-10-16 17:26:45 +0100

    Fix mistakes

--- tools/setup-phone.sh
+++ tools/setup-phone.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Setup script for getting phone to a convenient state for development work
 #
-cd build
+cd build-android
 rm lib/libmirclient.so.0
 phablet-flash ubuntu-system --no-backup --channel=devel-proposed
 adb shell mount -o remount,rw /
 adb shell touch /userdata/.writable_image
 phablet-network
-adb shell apt-get install mir-demos gdb
-adb shell sudo -i -u phablet stop unity8
+adb shell apt-get install -y mir-demos gdb
 ../tools/install_on_android.sh .
 
 # You now can do stuff like this:
