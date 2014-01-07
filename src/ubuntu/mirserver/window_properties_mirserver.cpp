e970ca1f020e01f06593896fda02725e37c25ebe
Gerry Boland <gerry.boland@canonical.com>
2014-01-07 11:00:33 +0000

    Fix FTBFS with Mir12 change - mir::geometry::PixelFormat was removed, using MirPixelFormat instead

--- src/ubuntu/mirserver/window_properties_mirserver.cpp
+++ src/ubuntu/mirserver/window_properties_mirserver.cpp
@@ -22,7 +22,6 @@
 
 namespace uams = ubuntu::application::mir::server;
 namespace msh = mir::shell;
-namespace geom = mir::geometry;
 
 uams::WindowProperties::WindowProperties()
     : parameters(msh::a_surface()),
@@ -52,11 +51,11 @@ void uams::WindowProperties::set_input_cb_and_ctx(UAUiWindowInputEventCb callbac
     input_ctx = ctx;
 }
 
-void uams::WindowProperties::set_pixel_format(geom::PixelFormat const& format)
+void uams::WindowProperties::set_pixel_format(MirPixelFormat const& format)
 {
     parameters = parameters.of_pixel_format(format);
 }
-    
+
 msh::SurfaceCreationParameters const& uams::WindowProperties::surface_parameters() const
 {
     return parameters;
