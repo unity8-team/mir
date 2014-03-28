6c94bb082d1cb116a971222d1d150d6636daf107
Gerry Boland <gerry.boland@canonical.com>
2014-03-28 17:00:25 +0000

    Add FIXME

--- src/platforms/mirserver/screen.cpp
+++ src/platforms/mirserver/screen.cpp
@@ -55,5 +55,5 @@ void Screen::readMirDisplayConfiguration(mir::graphics::DisplayConfigurationOutp
     m_geometry.setWidth(mode.size.width.as_int());
     m_geometry.setHeight(mode.size.height.as_int());
 
-    m_refreshRate = 60; //mode.vrefresh_hz value incorrect;
+    m_refreshRate = 60; //FIXME: mode.vrefresh_hz value seems to be incorrect??
 }
