b8842e0cd8a525b729f29eda69472b95d645a164
Alberto Aguirre <alberto.aguirre@canonical.com>
2014-04-09 12:59:38 -0500

    Create a random socket name to avoid collisions with other concurrent tests.

--- tests/mir_test_framework/testing_server_options.cpp
+++ tests/mir_test_framework/testing_server_options.cpp
@@ -17,14 +17,40 @@
  */
 
 #include "mir_test_framework/testing_server_configuration.h"
+#include "mir_test_framework/detect_server.h"
 
 #include "mir/server_status_listener.h"
 
 #include <boost/exception/errinfo_errno.hpp>
 #include <boost/throw_exception.hpp>
 
+#include <random>
+
 namespace mtf = mir_test_framework;
 
+namespace
+{
+std::string create_random_socket_name()
+{
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    int max_concurrent_test_instances = 99999;
+    std::uniform_int_distribution<> dist(1, max_concurrent_test_instances);
+    std::string const suffix{"-mir_socket_test"};
+
+    /* check for name collisions against other test instances
+     * running concurrently
+     */
+    for (int i = 0; i < max_concurrent_test_instances; i++)
+    {
+        std::string name{std::to_string(dist(generator))};
+        name.append(suffix);
+        if (!mtf::socket_exists(name))
+            return name;
+    }
+    throw std::runtime_error("Too many test socket instances exist!");
+}
+}
 
 mtf::TestingServerConfiguration::TestingServerConfiguration() :
     using_server_started_sync(false)
@@ -99,6 +125,6 @@ void mtf::TestingServerConfiguration::wait_for_server_start()
 
 std::string const& mtf::test_socket_file()
 {
-    static const std::string socket_file{"./mir_socket_test"};
+    static const std::string socket_file{create_random_socket_name()};
     return socket_file;
 }
