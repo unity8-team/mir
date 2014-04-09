b832d527182f5e3d0081882fdf1b5bc62ee721c5
Alberto Aguirre <alberto.aguirre@canonical.com>
2014-04-09 12:55:55 -0500

    Remove sigkill test

--- tests/acceptance-tests/test_server_startup.cpp
+++ tests/acceptance-tests/test_server_startup.cpp
@@ -49,47 +49,4 @@ TEST_F(BespokeDisplayServerTestFixture, server_announces_itself_on_startup)
 
     launch_client_process(client_config);
 }
-
-TEST_F(BespokeDisplayServerTestFixture, DISABLE_can_start_new_instance_after_sigkill)
-{
-    ASSERT_FALSE(mtf::detect_server(mtf::test_socket_file(), std::chrono::milliseconds(0)));
-
-    struct ServerConfig : TestingServerConfiguration
-    {
-        void on_start() override
-        {
-            sync.wait_for_signal_ready_for();
-            raise(SIGKILL);
-        }
-
-        mtf::CrossProcessSync sync;
-    };
-
-    ServerConfig server_crash_config;
-    launch_server_process(server_crash_config);
-
-    run_in_test_process([&]
-    {
-        server_crash_config.sync.signal_ready();
-
-        auto result = wait_for_shutdown_server_process();
-        EXPECT_EQ(mtf::TerminationReason::child_terminated_by_signal, result.reason);
-        EXPECT_TRUE(result.signal == SIGKILL);
-
-        //Now start a new server
-        TestingServerConfiguration server_config;
-        launch_server_process(server_config);
-
-        struct ClientConfig : TestingClientConfiguration
-        {
-            void exec()
-            {
-                EXPECT_TRUE(mtf::detect_server(mtf::test_socket_file(),
-                                               std::chrono::milliseconds(100)));
-            }
-        } client_config;
-
-        launch_client_process(client_config);
-    });
-}
 }
