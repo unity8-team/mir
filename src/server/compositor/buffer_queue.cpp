a9be0c4dd52f9807ce7d9f7f2b7d2ba93f527451
Daniel van Vugt <daniel.van.vugt@canonical.com>
2014-05-15 16:27:03 +0800

    More comments

--- src/server/compositor/buffer_queue.cpp
+++ src/server/compositor/buffer_queue.cpp
@@ -426,7 +426,7 @@ int mc::BufferQueue::min_buffers() const
     // else for multi-buffering with exclusivity guarantees:
     int client_demand = buffers_owned_by_client.size() +
                         pending_client_notifications.size();
-    // FIXME: compositor_demand is periodically spiking higher than reality
+    // FIXME: LP: #1308844 / LP: #1308843 is inflating compositor_demand
     int compositor_demand = buffers_sent_to_compositor.size();
     int min_compositors = std::max(1, compositor_demand);
     int min_clients = std::max(1, client_demand);
