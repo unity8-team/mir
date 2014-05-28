515d3bcc72a541dc749c8bed2f20ddfb546e9d82
Alan Griffiths <alan@octopull.co.uk>
2014-05-28 17:10:55 +0200

    Simplify signal/thread tests in test_client_library_threads.cpp

--- tests/acceptance-tests/test_client_library_threads.cpp
+++ tests/acceptance-tests/test_client_library_threads.cpp
@@ -21,8 +21,6 @@
 #include "mir_toolkit/mir_client_library.h"
 #include "mir_toolkit/mir_client_library_debug.h"
 
-#include "mir_protobuf.pb.h"
-
 #ifdef ANDROID
 /*
  * MirNativeBuffer for Android is defined opaquely, but we now depend on
@@ -32,99 +30,29 @@
 #endif
 
 #include <gtest/gtest.h>
-#include <chrono>
-#include <thread>
-#include <cstring>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#include <errno.h>
-
-namespace mf = mir::frontend;
-namespace mc = mir::compositor;
 namespace mtf = mir_test_framework;
 
 namespace
 {
     char const* const mir_test_socket = mtf::test_socket_file().c_str();
-}
-
-namespace mir
-{
-namespace
-{
-struct ClientConfigCommon : TestingClientConfiguration
-{
-    ClientConfigCommon()
-        : connection(0)
-        , surface(0),
-        buffers(0)
-    {
-    }
-
-    static void connection_callback(MirConnection * connection, void * context)
-    {
-        ClientConfigCommon * config = reinterpret_cast<ClientConfigCommon *>(context);
-        config->connection = connection;
-    }
-
-    static void create_surface_callback(MirSurface * surface, void * context)
-    {
-        ClientConfigCommon * config = reinterpret_cast<ClientConfigCommon *>(context);
-        config->surface_created(surface);
-    }
-
-    static void next_buffer_callback(MirSurface * surface, void * context)
-    {
-        ClientConfigCommon * config = reinterpret_cast<ClientConfigCommon *>(context);
-        config->next_buffer(surface);
-    }
-
-    static void release_surface_callback(MirSurface * surface, void * context)
-    {
-        ClientConfigCommon * config = reinterpret_cast<ClientConfigCommon *>(context);
-        config->surface_released(surface);
-    }
-
-    virtual void connected(MirConnection * new_connection)
-    {
-        connection = new_connection;
-    }
-
-    virtual void surface_created(MirSurface * new_surface)
-    {
-        surface = new_surface;
-    }
-
-    virtual void next_buffer(MirSurface*)
-    {
-        ++buffers;
-    }
-
-    virtual void surface_released(MirSurface * /*released_surface*/)
-    {
-        surface = NULL;
-    }
-
-    MirConnection* connection;
-    MirSurface* surface;
-    int buffers;
-};
-}
 
 bool signalled;
 static void SIGIO_handler(int /*signo*/)
 {
     signalled = true;
 }
+}
 
 using ClientLibraryThreads = DefaultDisplayServerTestFixture;
 
 TEST_F(ClientLibraryThreads, HandleNoSignals)
 {
-    struct ClientConfig : ClientConfigCommon
+    struct ClientConfig : TestingClientConfiguration
     {
         void exec()
         {
@@ -140,8 +68,7 @@ TEST_F(ClientLibraryThreads, HandleNoSignals)
             if (sigaction(SIGIO, &act, NULL))
                 FAIL() << "Failed to set SIGIO action";
 
-            MirConnection* conn = NULL;
-            conn = mir_connect_sync(mir_test_socket, __PRETTY_FUNCTION__);
+            MirConnection* conn = mir_connect_sync(mir_test_socket, __PRETTY_FUNCTION__);
 
             sigaddset(&sigset, SIGIO);
             pthread_sigmask(SIG_BLOCK, &sigset, NULL);
@@ -160,7 +87,7 @@ TEST_F(ClientLibraryThreads, HandleNoSignals)
                 mir_display_output_id_invalid
             };
 
-            surface = mir_connection_create_surface_sync(conn, &request_params);
+            mir_connection_create_surface_sync(conn, &request_params);
 
             mir_connection_release(conn);
 
@@ -173,7 +100,7 @@ TEST_F(ClientLibraryThreads, HandleNoSignals)
 
 TEST_F(ClientLibraryThreads, DoNotInterfereWithClientSignalHandling)
 {
-    struct ClientConfig : ClientConfigCommon
+    struct ClientConfig : TestingClientConfiguration
     {
         void exec()
         {
@@ -189,8 +116,7 @@ TEST_F(ClientLibraryThreads, DoNotInterfereWithClientSignalHandling)
             if (sigaction(SIGIO, &act, NULL))
                 FAIL() << "Failed to set SIGIO action";
 
-            MirConnection* conn = NULL;
-            conn = mir_connect_sync(mir_test_socket, __PRETTY_FUNCTION__);
+            MirConnection* conn = mir_connect_sync(mir_test_socket, __PRETTY_FUNCTION__);
 
             // We should receieve SIGIO
             if (kill(getpid(), SIGIO))
@@ -203,5 +129,4 @@ TEST_F(ClientLibraryThreads, DoNotInterfereWithClientSignalHandling)
     } client_config;
 
     launch_client_process(client_config);
-}
 }
