cb8356957f37964a114f3d5436f32198cc3952f4
Alan Griffiths <alan@octopull.co.uk>
2014-05-28 15:25:23 +0200

    Tidy up remaining tests

--- tests/acceptance-tests/test_client_library.cpp
+++ tests/acceptance-tests/test_client_library.cpp
@@ -383,8 +383,6 @@ TEST_F(ClientLibrary, receives_surface_state_events)
 TEST_F(ClientLibrary, receives_surface_dpi_value)
 {
     connection = mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__);
-    ASSERT_TRUE(connection != NULL);
-    ASSERT_TRUE(mir_connection_is_valid(connection));
 
     MirSurfaceParameters const request_params =
     {
@@ -395,13 +393,10 @@ TEST_F(ClientLibrary, receives_surface_dpi_value)
         mir_display_output_id_invalid
     };
 
-    surface =
-        mir_connection_create_surface_sync(connection, &request_params);
-    ASSERT_TRUE(surface != NULL);
-    ASSERT_TRUE(mir_surface_is_valid(surface));
+    surface = mir_connection_create_surface_sync(connection, &request_params);
 
     // Expect zero (not wired up to detect the physical display yet)
-    EXPECT_EQ(0, mir_surface_get_dpi(surface));
+    EXPECT_THAT(mir_surface_get_dpi(surface), Eq(0));
 
     mir_surface_release_sync(surface);
     mir_connection_release(connection);
@@ -411,9 +406,6 @@ TEST_F(ClientLibrary, receives_surface_dpi_value)
 TEST_F(ClientLibrary, surface_scanout_flag_toggles)
 {
     connection = mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__);
-    ASSERT_TRUE(connection != NULL);
-    EXPECT_TRUE(mir_connection_is_valid(connection));
-    EXPECT_STREQ(mir_connection_get_error_message(connection), "");
 
     MirSurfaceParameters parm =
     {
@@ -425,7 +417,7 @@ TEST_F(ClientLibrary, surface_scanout_flag_toggles)
     };
 
     surface = mir_connection_create_surface_sync(connection, &parm);
-    ASSERT_TRUE(mir_surface_is_valid(surface));
+
     MirNativeBuffer *native;
     mir_surface_get_current_buffer(surface, &native);
     EXPECT_TRUE(native->flags & mir_buffer_flag_can_scanout);
@@ -437,7 +429,6 @@ TEST_F(ClientLibrary, surface_scanout_flag_toggles)
     parm.height = 100;
 
     surface = mir_connection_create_surface_sync(connection, &parm);
-    ASSERT_TRUE(mir_surface_is_valid(surface));
     mir_surface_get_current_buffer(surface, &native);
     EXPECT_FALSE(native->flags & mir_buffer_flag_can_scanout);
     mir_surface_swap_buffers_sync(surface);
@@ -449,7 +440,6 @@ TEST_F(ClientLibrary, surface_scanout_flag_toggles)
     parm.buffer_usage = mir_buffer_usage_software;
 
     surface = mir_connection_create_surface_sync(connection, &parm);
-    ASSERT_TRUE(mir_surface_is_valid(surface));
     mir_surface_get_current_buffer(surface, &native);
     EXPECT_FALSE(native->flags & mir_buffer_flag_can_scanout);
     mir_surface_swap_buffers_sync(surface);
@@ -459,7 +449,6 @@ TEST_F(ClientLibrary, surface_scanout_flag_toggles)
     parm.buffer_usage = mir_buffer_usage_hardware;
 
     surface = mir_connection_create_surface_sync(connection, &parm);
-    ASSERT_TRUE(mir_surface_is_valid(surface));
     mir_surface_get_current_buffer(surface, &native);
     EXPECT_TRUE(native->flags & mir_buffer_flag_can_scanout);
     mir_surface_swap_buffers_sync(surface);
@@ -478,8 +467,6 @@ TEST_F(ClientLibrary, gets_buffer_dimensions)
 #endif
 {
     connection = mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__);
-    ASSERT_TRUE(connection != NULL);
-    ASSERT_TRUE(mir_connection_is_valid(connection));
 
     MirSurfaceParameters parm =
     {
@@ -503,20 +490,18 @@ TEST_F(ClientLibrary, gets_buffer_dimensions)
         parm.height = size.height;
 
         surface = mir_connection_create_surface_sync(connection, &parm);
-        ASSERT_TRUE(surface != NULL);
-        ASSERT_TRUE(mir_surface_is_valid(surface));
 
         MirNativeBuffer *native = NULL;
         mir_surface_get_current_buffer(surface, &native);
-        ASSERT_TRUE(native != NULL);
-        EXPECT_EQ(parm.width, native->width);
-        EXPECT_EQ(parm.height, native->height);
+        ASSERT_THAT(native, NotNull());
+        EXPECT_THAT(native->width, Eq(parm.width));
+        ASSERT_THAT(native->height, Eq(parm.height));
 
         mir_surface_swap_buffers_sync(surface);
         mir_surface_get_current_buffer(surface, &native);
-        ASSERT_TRUE(native != NULL);
-        EXPECT_EQ(parm.width, native->width);
-        EXPECT_EQ(parm.height, native->height);
+        ASSERT_THAT(native, NotNull());
+        EXPECT_THAT(native->width, Eq(parm.width));
+        ASSERT_THAT(native->height, Eq(parm.height));
 
         mir_surface_release_sync(surface);
     }
@@ -530,10 +515,6 @@ TEST_F(ClientLibrary, creates_multiple_surfaces)
     size_t old_surface_count = 0;
 
     mir_wait_for(mir_connect(new_connection().c_str(), __PRETTY_FUNCTION__, connection_callback, this));
-
-    ASSERT_TRUE(connection != NULL);
-    EXPECT_TRUE(mir_connection_is_valid(connection));
-    EXPECT_STREQ(mir_connection_get_error_message(connection), "");
 
     for (int i = 0; i != n_surfaces; ++i)
     {
@@ -550,18 +531,18 @@ TEST_F(ClientLibrary, creates_multiple_surfaces)
 
         mir_wait_for(mir_connection_create_surface(connection, &request_params, create_surface_callback, this));
 
-        ASSERT_EQ(old_surface_count + 1, current_surface_count());
+        ASSERT_THAT(current_surface_count(), Eq(old_surface_count + 1));
     }
     for (int i = 0; i != n_surfaces; ++i)
     {
         old_surface_count = current_surface_count();
 
-        ASSERT_NE(old_surface_count, 0u);
+        ASSERT_THAT(old_surface_count, Ne(0u));
 
         MirSurface * surface = any_surface();
         mir_wait_for(mir_surface_release( surface, release_surface_callback, this));
 
-        ASSERT_EQ(old_surface_count - 1, current_surface_count());
+        ASSERT_THAT(current_surface_count(), Eq(old_surface_count - 1));
     }
 
     mir_connection_release(connection);
@@ -570,10 +551,6 @@ TEST_F(ClientLibrary, creates_multiple_surfaces)
 TEST_F(ClientLibrary, client_library_accesses_and_advances_buffers)
 {
     mir_wait_for(mir_connect(new_connection().c_str(), __PRETTY_FUNCTION__, connection_callback, this));
-
-    ASSERT_TRUE(connection != NULL);
-    EXPECT_TRUE(mir_connection_is_valid(connection));
-    EXPECT_STREQ("", mir_connection_get_error_message(connection));
 
     MirSurfaceParameters const request_params =
     {
@@ -585,27 +562,21 @@ TEST_F(ClientLibrary, client_library_accesses_and_advances_buffers)
     };
 
     mir_wait_for(mir_connection_create_surface(connection, &request_params, create_surface_callback, this));
-    ASSERT_TRUE(surface != NULL);
 
     buffers = 0;
     mir_wait_for(mir_surface_swap_buffers(surface, next_buffer_callback, this));
-    EXPECT_EQ(buffers, 1);
+    EXPECT_THAT(buffers, Eq(1));
 
-    mir_wait_for(mir_surface_release( surface, release_surface_callback, this));
+    mir_wait_for(mir_surface_release(surface, release_surface_callback, this));
 
-    ASSERT_TRUE(surface == NULL);
+    ASSERT_THAT(surface, IsNull());
 
     mir_connection_release(connection);
 }
 
 TEST_F(ClientLibrary, fully_synchronous_client)
 {
-    connection = mir_connect_sync(new_connection().c_str(),
-                                  __PRETTY_FUNCTION__);
-
-    ASSERT_TRUE(connection != NULL);
-    EXPECT_TRUE(mir_connection_is_valid(connection));
-    EXPECT_STREQ("", mir_connection_get_error_message(connection));
+    connection = mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__);
 
     MirSurfaceParameters const request_params =
     {
@@ -617,9 +588,6 @@ TEST_F(ClientLibrary, fully_synchronous_client)
     };
 
     surface = mir_connection_create_surface_sync(connection, &request_params);
-    ASSERT_TRUE(surface != NULL);
-    EXPECT_TRUE(mir_surface_is_valid(surface));
-    EXPECT_STREQ(mir_surface_get_error_message(surface), "");
 
     mir_surface_swap_buffers_sync(surface);
     EXPECT_TRUE(mir_surface_is_valid(surface));
@@ -634,12 +602,7 @@ TEST_F(ClientLibrary, fully_synchronous_client)
 
 TEST_F(ClientLibrary, highly_threaded_client)
 {
-    connection = mir_connect_sync(new_connection().c_str(),
-                                  __PRETTY_FUNCTION__);
-
-    ASSERT_TRUE(connection != NULL);
-    EXPECT_TRUE(mir_connection_is_valid(connection));
-    EXPECT_STREQ("", mir_connection_get_error_message(connection));
+    connection = mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__);
 
     MirSurfaceParameters const request_params =
     {
@@ -650,11 +613,7 @@ TEST_F(ClientLibrary, highly_threaded_client)
         mir_display_output_id_invalid
     };
 
-    surface = mir_connection_create_surface_sync(connection,
-                                                 &request_params);
-    ASSERT_TRUE(surface != NULL);
-    EXPECT_TRUE(mir_surface_is_valid(surface));
-    EXPECT_STREQ(mir_surface_get_error_message(surface), "");
+    surface = mir_connection_create_surface_sync(connection, &request_params);
 
     std::thread a(nosey_thread, surface);
     std::thread b(nosey_thread, surface);
@@ -664,22 +623,19 @@ TEST_F(ClientLibrary, highly_threaded_client)
     b.join();
     c.join();
 
-    EXPECT_EQ(mir_surface_type_dialog,
-              mir_surface_get_type(surface));
-    EXPECT_EQ(mir_surface_state_minimized,
-              mir_surface_get_state(surface));
+    EXPECT_THAT(mir_surface_get_type(surface), Eq(mir_surface_type_dialog));
+    EXPECT_THAT(mir_surface_get_type(surface), Eq(mir_surface_state_minimized));
 
     mir_surface_release_sync(surface);
 
     EXPECT_TRUE(mir_connection_is_valid(connection));
-    EXPECT_STREQ("", mir_connection_get_error_message(connection));
+    EXPECT_THAT(mir_connection_get_error_message(connection), StrEq(""));
     mir_connection_release(connection);
 }
 
 TEST_F(ClientLibrary, accesses_platform_package)
 {
     mir_wait_for(mir_connect(new_connection().c_str(), __PRETTY_FUNCTION__, connection_callback, this));
-    ASSERT_TRUE(connection != NULL);
 
     MirPlatformPackage platform_package;
     platform_package.data_items = -1;
@@ -695,16 +651,15 @@ TEST_F(ClientLibrary, accesses_platform_package)
 TEST_F(ClientLibrary, accesses_display_info)
 {
     mir_wait_for(mir_connect(new_connection().c_str(), __PRETTY_FUNCTION__, connection_callback, this));
-    ASSERT_TRUE(connection != NULL);
 
     auto configuration = mir_connection_create_display_config(connection);
-    ASSERT_NE(nullptr, configuration);
+    ASSERT_THAT(configuration, NotNull());
     ASSERT_GT(configuration->num_outputs, 0u);
-    ASSERT_NE(nullptr, configuration->outputs);
+    ASSERT_THAT(configuration->outputs, NotNull());
     for (auto i=0u; i < configuration->num_outputs; i++)
     {
         MirDisplayOutput* disp = &configuration->outputs[i];
-        ASSERT_NE(nullptr, disp);
+        ASSERT_THAT(disp, NotNull());
         EXPECT_GE(disp->num_modes, disp->current_mode);
         EXPECT_GE(disp->num_output_formats, disp->current_format);
     }
@@ -716,7 +671,7 @@ TEST_F(ClientLibrary, accesses_display_info)
 TEST_F(ClientLibrary, connect_errors_handled)
 {
     mir_wait_for(mir_connect("garbage", __PRETTY_FUNCTION__, connection_callback, this));
-    ASSERT_TRUE(connection != NULL);
+    ASSERT_THAT(connection, NotNull());
 
     char const* error = mir_connection_get_error_message(connection);
 
@@ -753,10 +708,6 @@ TEST_F(ClientLibrary, MultiSurfaceClientTracksBufferFdsCorrectly)
 {
     mir_wait_for(mir_connect(new_connection().c_str(), __PRETTY_FUNCTION__, connection_callback, this));
 
-    ASSERT_TRUE(connection != NULL);
-    EXPECT_TRUE(mir_connection_is_valid(connection));
-    EXPECT_STREQ("", mir_connection_get_error_message(connection));
-
     MirSurfaceParameters const request_params =
     {
         __PRETTY_FUNCTION__,
@@ -769,8 +720,8 @@ TEST_F(ClientLibrary, MultiSurfaceClientTracksBufferFdsCorrectly)
     MirSurface* surf_one = mir_connection_create_surface_sync(connection, &request_params);
     MirSurface* surf_two = mir_connection_create_surface_sync(connection, &request_params);
 
-    ASSERT_TRUE(surf_one != NULL);
-    ASSERT_TRUE(surf_two != NULL);
+    ASSERT_THAT(surf_one, NotNull());
+    ASSERT_THAT(surf_two, NotNull());
 
     buffers = 0;
 
@@ -787,8 +738,8 @@ TEST_F(ClientLibrary, MultiSurfaceClientTracksBufferFdsCorrectly)
     int canary_fd;
     canary_fd = open("/dev/null", O_RDONLY);
 
-    ASSERT_TRUE(canary_fd > 0) << "Failed to open canary file descriptor: "<< strerror(errno);
-    EXPECT_TRUE(canary_fd < 1024);
+    ASSERT_THAT(canary_fd, Gt(0)) << "Failed to open canary file descriptor: "<< strerror(errno);
+    EXPECT_THAT(canary_fd, Lt(1024));
 
     close(canary_fd);
 
