4fd676fc1a44284bd6e8e72ba99a80175a367dd7
Gerry Boland <gerry.boland@canonical.com>
2014-07-01 18:32:15 +0100

    Actually, set application running in setFocused only if app is not in starting state

--- src/modules/Unity/Application/application_manager.cpp
+++ src/modules/Unity/Application/application_manager.cpp
@@ -812,6 +812,8 @@ void ApplicationManager::setFocused(Application *application)
 
     m_focusedApplication = application;
     m_focusedApplication->setFocused(true);
+    if (m_focusedApplication->state() != Application::Starting)
+        m_focusedApplication->setState(Application::Running);
     move(m_applications.indexOf(application), 0);
     Q_EMIT focusedApplicationIdChanged();
     m_dbusWindowStack->FocusedWindowChanged(0, application->appId(), application->stage());
