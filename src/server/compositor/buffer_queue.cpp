29dede0da054eea9518a8471d8faa279249a46dc
Daniel van Vugt <daniel.van.vugt@canonical.com>
2014-07-09 15:16:08 +0800

    Reenable debug output

--- src/server/compositor/buffer_queue.cpp
+++ src/server/compositor/buffer_queue.cpp
@@ -219,7 +219,7 @@ mc::BufferQueue::compositor_acquire(void const* user_id)
 {
     std::unique_lock<decltype(guard)> lock(guard);
 
-    //fprintf(stderr, "#buffers = %d\n", (int)buffers.size());
+    fprintf(stderr, "#buffers = %d\n", (int)buffers.size());
 
     bool use_current_buffer = false;
     if (!current_buffer_users.empty() && !is_a_current_buffer_user(user_id))
@@ -483,7 +483,7 @@ int mc::BufferQueue::min_buffers() const
 
     // FIXME: Sometimes required_buffers > nbuffers (LP: #1317403)
     int ret = std::min(nbuffers, required_buffers);
-    //fprintf(stderr, "min_buffers = %d\n", ret);
+    fprintf(stderr, "min_buffers = %d\n", ret);
     return ret;
 }
 
