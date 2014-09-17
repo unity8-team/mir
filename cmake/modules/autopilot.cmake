ae58711c7aa0cf34ec791543ef07747e4f3713c6
Robert Carr <robert.carr@canonical.com>
2014-09-17 13:40:39 -0700

    Begin on cmake

---
+++ cmake/modules/autopilot.cmake
@@ -0,0 +1,24 @@
+add_custom_target(autopilot)
+
+function(declare_autopilot_test TEST_NAME TEST_SUITE WORKING_DIR)
+    add_custom_target(autopilot-${TEST_NAME}
+        COMMAND LANG=C UBUNTU_ICON_THEME=ubuntu-mobile QML2_IMPORT_PATH=${SHELL_INSTALL_QML}/mocks python3 -m autopilot.run run ${TEST_SUITE}
+        WORKING_DIRECTORY ${WORKING_DIR}
+        DEPENDS fake_install
+    )
+
+    add_custom_target(fake_install
+        COMMAND cmake --build ${CMAKE_BINARY_DIR} --target install
+    )
+
+    add_dependencies(autopilot autopilot-${TEST_NAME})
+
+    add_custom_target(autopilot2-${TEST_NAME}
+        COMMAND LANG=C UBUNTU_ICON_THEME=ubuntu-mobile QML2_IMPORT_PATH=${SHELL_INSTALL_QML}/mocks python2 -m autopilot.run run ${TEST_SUITE}
+        WORKING_DIRECTORY ${WORKING_DIR}
+        DEPENDS fake_install
+    )
+
+    add_dependencies(autopilot autopilot2-${TEST_NAME})
+
+endfunction()
