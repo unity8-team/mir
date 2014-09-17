41182b38a1d161456b1cc566ea7b25a0377c8c4f
Gerry Boland <gerry.boland@canonical.com>
2014-09-17 18:29:42 +0100

    Rewrite of DesktopFileReader to use the GIO GDesktopAppInfo "class"

--- src/modules/Unity/Application/desktopfilereader.cpp
+++ src/modules/Unity/Application/desktopfilereader.cpp
@@ -16,142 +16,167 @@
 
 // local
 #include "desktopfilereader.h"
+#include "gscopedpointer.h"
 #include "logging.h"
 
 // Qt
 #include <QFile>
 
+// GIO
+#include <gio/gdesktopappinfo.h>
+
 namespace qtmir
 {
 
-DesktopFileReader::Factory::Factory()
-{
-}
-
-DesktopFileReader::Factory::~Factory()
-{
-}
 
 DesktopFileReader* DesktopFileReader::Factory::createInstance(const QString &appId, const QFileInfo& fi)
 {
     return new DesktopFileReader(appId, fi);
 }
 
-// Retrieves the size of an array at compile time.
-#define ARRAY_SIZE(a) \
-    ((sizeof(a) / sizeof(*(a))) / static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
+typedef GObjectScopedPointer<GDesktopAppInfo> GDesktopAppInfoPointer;
+
+struct DesktopFileReaderPrivate
+{
+    DesktopFileReaderPrivate(DesktopFileReader *parent):
+            q_ptr( parent )
+    {}
+
+    QString getKey(const char *key) const
+    {
+        if (!loaded()) return QString();
+
+        return QString::fromUtf8(g_desktop_app_info_get_string(appInfo.data(), key));
+    }
+
+    bool loaded() const
+    {
+        return !appInfo.isNull();
+    }
+
+    DesktopFileReader * const q_ptr;
+    Q_DECLARE_PUBLIC(DesktopFileReader)
+
+    QString appId;
+    QString file;
+    GDesktopAppInfoPointer appInfo;
+};
+
 
 DesktopFileReader::DesktopFileReader(const QString &appId, const QFileInfo &desktopFile)
-    : appId_(appId)
-    , entries_(DesktopFileReader::kNumberOfEntries, "")
+    : d_ptr(new DesktopFileReaderPrivate(this))
 {
     qCDebug(QTMIR_APPLICATIONS) << "DesktopFileReader::DesktopFileReader - this=" << this << "appId=" << appId;
+    Q_D(DesktopFileReader);
 
-    file_ = desktopFile.absoluteFilePath();
-    loaded_ = loadDesktopFile(file_);
+    d->appId = appId;
+    d->file = desktopFile.absoluteFilePath();
+    d->appInfo.reset(g_desktop_app_info_new_from_filename(d->file.toUtf8().constData()));
+
+    if (!d->loaded()) {
+        qWarning() << "Desktop file for appId:" << appId << "at:" << d->file << "does not exist, or is not valid";
+    }
 }
 
 DesktopFileReader::~DesktopFileReader()
 {
-    qCDebug(QTMIR_APPLICATIONS) << "DesktopFileReader::~DesktopFileReader";
-    entries_.clear();
+    Q_D(const DesktopFileReader);
+    qCDebug(QTMIR_APPLICATIONS) << "DesktopFileReader::~DesktopFileReader - this=" << this << "appId=" << d->appId;
+    delete d_ptr;
 }
 
-bool DesktopFileReader::loadDesktopFile(QString desktopFile)
+QString DesktopFileReader::file() const
 {
-    qCDebug(QTMIR_APPLICATIONS) << "DesktopFileReader::loadDesktopFile - this=" << this << "desktopFile=" << desktopFile;
+    Q_D(const DesktopFileReader);
+    return d->file;
+}
 
-    if (this->file().isNull() || this->file().isEmpty()) {
-        qCritical() << "No desktop file found for appId:" << appId_;
-        return false;
-    }
+QString DesktopFileReader::appId() const
+{
+    Q_D(const DesktopFileReader);
+    return d->appId;
+}
 
-    Q_ASSERT(desktopFile != NULL);
-    const struct { const char* const name; int size; unsigned int flag; } kEntryNames[] = {
-        { "Name=", sizeof("Name=") - 1, 1 << DesktopFileReader::kNameIndex },
-        { "Comment=", sizeof("Comment=") - 1, 1 << DesktopFileReader::kCommentIndex },
-        { "Icon=", sizeof("Icon=") - 1, 1 << DesktopFileReader::kIconIndex },
-        { "Exec=", sizeof("Exec=") - 1, 1 << DesktopFileReader::kExecIndex },
-        { "Path=", sizeof("Path=") - 1, 1 << DesktopFileReader::kPathIndex },
-        { "X-Ubuntu-StageHint=", sizeof("X-Ubuntu-StageHint=") - 1, 1 << DesktopFileReader::kStageHintIndex },
-        { "X-Ubuntu-Splash-Title=", sizeof("X-Ubuntu-Splash-Title=") - 1, 1 << DesktopFileReader::kSplashTitleIndex },
-        { "X-Ubuntu-Splash-Image=", sizeof("X-Ubuntu-Splash-Image=") - 1, 1 << DesktopFileReader::kSplashImageIndex },
-        { "X-Ubuntu-Splash-Show-Header=", sizeof("X-Ubuntu-Splash-Show-Header=") - 1, 1 << DesktopFileReader::kSplashShowHeaderIndex },
-        { "X-Ubuntu-Splash-Color=", sizeof("X-Ubuntu-Splash-Color=") - 1, 1 << DesktopFileReader::kSplashColorIndex },
-        { "X-Ubuntu-Splash-Color-Header=", sizeof("X-Ubuntu-Splash-Color-Header=") - 1, 1 << DesktopFileReader::kSplashColorHeaderIndex },
-        { "X-Ubuntu-Splash-Color-Footer=", sizeof("X-Ubuntu-Splash-Color-Footer=") - 1, 1 << DesktopFileReader::kSplashColorFooterIndex }
-    };
-    const unsigned int kAllEntriesMask =
-            (1 << DesktopFileReader::kNameIndex) | (1 << DesktopFileReader::kCommentIndex)
-            | (1 << DesktopFileReader::kIconIndex) | (1 << DesktopFileReader::kExecIndex)
-            | (1 << DesktopFileReader::kPathIndex) | (1 << DesktopFileReader::kStageHintIndex)
-            | (1 << DesktopFileReader::kSplashTitleIndex) | (1 << DesktopFileReader::kSplashImageIndex)
-            | (1 << DesktopFileReader::kSplashShowHeaderIndex) | (1 << DesktopFileReader::kSplashColorIndex)
-            | (1 << DesktopFileReader::kSplashColorHeaderIndex) | (1 << DesktopFileReader::kSplashColorFooterIndex);
-    const unsigned int kMandatoryEntriesMask =
-            (1 << DesktopFileReader::kNameIndex) | (1 << DesktopFileReader::kIconIndex)
-            | (1 << DesktopFileReader::kExecIndex);
-    const int kEntriesCount = ARRAY_SIZE(kEntryNames);
-    const int kBufferSize = 256;
-    static char buffer[kBufferSize];
-    QFile file(desktopFile);
+QString DesktopFileReader::name() const
+{
+    Q_D(const DesktopFileReader);
+    if (!d->loaded()) return QString();
 
-    // Open file.
-    if (!file.open(QFile::ReadOnly | QIODevice::Text)) {
-        qWarning() << "Can't open file:" << file.errorString();
-        return false;
-    }
+    return d->getKey("Name"); //QString::fromUtf8(g_app_info_get_name(d->appInfo.data()));
+}
 
-    // Validate "magic key" (standard group header).
-    if (file.readLine(buffer, kBufferSize) != -1) {
-        if (strncmp(buffer, "[Desktop Entry]", sizeof("[Desktop Entry]") - 1)) {
-            qWarning() << "not a desktop file, unable to read it";
-            return false;
-        }
-    }
+QString DesktopFileReader::comment() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("Comment");
+}
 
-    int length;
-    unsigned int entryFlags = 0;
-    while ((length = file.readLine(buffer, kBufferSize)) != -1) {
-        // Skip empty lines.
-        if (length > 1) {
-            // Stop when reaching unsupported next group header.
-            if (buffer[0] == '[') {
-                qWarning() << "reached next group header, leaving loop";
-                break;
-            }
-            // Lookup entries ignoring duplicates if any.
-            for (int i = 0; i < kEntriesCount; i++) {
-                if (!strncmp(buffer, kEntryNames[i].name, kEntryNames[i].size)) {
-                    if (~entryFlags & kEntryNames[i].flag) {
-                        buffer[length-1] = '\0';
-                        entries_[i] = QString::fromUtf8(&buffer[kEntryNames[i].size]);
-                        entryFlags |= kEntryNames[i].flag;
-                        break;
-                    }
-                }
-            }
-            // Stop when matching the right number of entries.
-            if (entryFlags == kAllEntriesMask) {
-                break;
-            }
-        }
-    }
+QString DesktopFileReader::icon() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("Icon");
+}
 
-    // Check that the mandatory entries are set.
-    if ((entryFlags & kMandatoryEntriesMask) == kMandatoryEntriesMask) {
-        qDebug("loaded desktop file with name='%s', comment='%s', icon='%s', exec='%s', path='%s', stagehint='%s'",
-                qPrintable(entries_[DesktopFileReader::kNameIndex]),
-                qPrintable(entries_[DesktopFileReader::kCommentIndex]),
-                qPrintable(entries_[DesktopFileReader::kIconIndex]),
-                qPrintable(entries_[DesktopFileReader::kExecIndex]),
-                qPrintable(entries_[DesktopFileReader::kPathIndex]),
-                qPrintable(entries_[DesktopFileReader::kStageHintIndex]));
-        return true;
-    } else {
-        qWarning() << "not a valid desktop file, missing mandatory entries in the standard group header";
-        return false;
-    }
+QString DesktopFileReader::exec() const
+{
+    Q_D(const DesktopFileReader);
+    if (!d->loaded()) return QString();
+
+    return d->getKey("Exec"); // QString::fromUtf8(g_app_info_get_executable(d->appInfo.data()));
+}
+
+QString DesktopFileReader::path() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("Path");
+}
+
+QString DesktopFileReader::stageHint() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("X-Ubuntu-StageHint");
+}
+
+QString DesktopFileReader::splashTitle() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("X-Ubuntu-Splash-Title");
+}
+
+QString DesktopFileReader::splashImage() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("X-Ubuntu-Splash-Image");
+}
+
+QString DesktopFileReader::splashShowHeader() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("X-Ubuntu-Splash-Show-Header");
+}
+
+QString DesktopFileReader::splashColor() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("X-Ubuntu-Splash-Color");
+}
+
+QString DesktopFileReader::splashColorHeader() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("X-Ubuntu-Splash-Color-Header");
+}
+
+QString DesktopFileReader::splashColorFooter() const
+{
+    Q_D(const DesktopFileReader);
+    return d->getKey("X-Ubuntu-Splash-Color-Footer");
+}
+
+bool DesktopFileReader::loaded() const
+{
+    Q_D(const DesktopFileReader);
+    return d->loaded();
 }
 
 } // namespace qtmir
