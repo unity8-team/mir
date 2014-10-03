8aa3b4bbba3e1f1132ca5380eb2735368cdb44f7
Albert Astals <albert.astals@canonical.com>
2014-10-03 16:59:27 +0200

    move up

--- src/platforms/mirserver/sessionauthorizer.cpp
+++ src/platforms/mirserver/sessionauthorizer.cpp
@@ -43,6 +43,7 @@ bool SessionAuthorizer::connection_is_allowed(SessionCredentials const& creds)
     bool authorized = true;
 
     if (!m_connectionChecked) {
+        m_connectionChecked = true;
         // Wait until the ApplicationManager is ready to receive requestAuthorizationForSession signals
         const QMetaObject *mo = metaObject();
         QMetaMethod mm = mo->method(mo->indexOfSignal("requestAuthorizationForSession(quint64,bool&)"));
@@ -53,7 +54,6 @@ bool SessionAuthorizer::connection_is_allowed(SessionCredentials const& creds)
             qCDebug(QTMIR_MIR_MESSAGES) <<
                 "SessionAuthorizer::connection_is_allowed - Gave up waiting for signal listeners";
         }
-        m_connectionChecked = true;
     }
 
     Q_EMIT requestAuthorizationForSession(creds.pid(), authorized); // needs to block until authorized value returned
