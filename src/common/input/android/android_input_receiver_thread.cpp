031324a8d48d219dca4f6d05cf831671df30b261
Robert Carr <robert.carr@canonical.com>
2014-10-17 00:33:50 -0700

    Sweet hacks

--- src/common/input/android/android_input_receiver_thread.cpp
+++ src/common/input/android/android_input_receiver_thread.cpp
@@ -61,7 +61,7 @@ void mircva::InputReceiverThread::thread_loop()
 {
     while (running)
     {
-        std::unique_lock<std::recursive_mutex> lg(frame_time_mutex);
+        std::unique_lock<std::mutex> lg(frame_time_mutex);
         MirEvent ev;
         nsecs_t frame_time_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(last_frame_time.time_since_epoch()).count();
         // TODO: Pass time to receiver
@@ -70,7 +70,11 @@ void mircva::InputReceiverThread::thread_loop()
             frame_time_ns = -1;
 
         while(running && receiver->next_event(ev, frame_time_ns))
+            {
+                lg.unlock();
             handler(&ev);
+            lg.lock();
+            }
 
         // TODO: Hack
 //        last_frame_time = std::chrono::high_resolution_clock::now();
@@ -83,7 +87,7 @@ void mircva::InputReceiverThread::notify_of_frame_start(std::chrono::high_resolu
 {
     (void) frame_time;
     {
-        std::lock_guard<std::recursive_mutex> lg(frame_time_mutex);
+        std::lock_guard<std::mutex> lg(frame_time_mutex);
         last_frame_time = frame_time;
     }
   frame_cv.notify_all();
