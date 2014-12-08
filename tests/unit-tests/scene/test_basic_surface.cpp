fca03d7a714b14ac880cdf1f5c185eaef72e1aa6
Daniel van Vugt <daniel.van.vugt@canonical.com>
2014-12-08 14:43:20 +0800

    Fix fragile BasicSurfaceTest's

--- tests/unit-tests/scene/test_basic_surface.cpp
+++ tests/unit-tests/scene/test_basic_surface.cpp
@@ -115,6 +115,14 @@ struct BasicSurfaceTest : public testing::Test
         report};
 };
 
+void post_a_frame(ms::BasicSurface& surface)
+{
+    // Make sure there's a frame ready. Otherwise visible()==false and the
+    // input_area will never report it containing anything.
+    mtd::StubBuffer buffer;
+    surface.swap_buffers(&buffer, [&](mir::graphics::Buffer*){});
+}
+
 }
 
 TEST_F(BasicSurfaceTest, basics)
@@ -264,7 +272,7 @@ TEST_F(BasicSurfaceTest, test_surface_visibility)
     mir::graphics::Buffer* buffer = nullptr;
     auto const callback = [&](mir::graphics::Buffer* new_buffer) { buffer = new_buffer; };
 
-    //not visible by default
+    //not visible by default  -  FIXME: This assumes no other tests ran yet
     EXPECT_FALSE(surface.visible());
 
     surface.set_hidden(false);
@@ -330,6 +338,7 @@ TEST_F(BasicSurfaceTest, default_region_is_surface_rectangle)
         report};
 
     surface.add_observer(observer);
+    post_a_frame(surface);
 
     std::vector<geom::Point> contained_pt
     {
@@ -362,6 +371,7 @@ TEST_F(BasicSurfaceTest, set_input_region)
     };
 
     surface.add_observer(observer);
+    post_a_frame(surface);
 
     surface.set_input_region(rectangles);
 
@@ -395,6 +405,7 @@ TEST_F(BasicSurfaceTest, updates_default_input_region_when_surface_is_resized_to
 {
     geom::Rectangle const new_rect{rect.top_left,{10,10}};
     surface.resize(new_rect.size);
+    post_a_frame(surface);
 
     for (auto x = new_rect.top_left.x.as_int() - 1;
          x <= new_rect.top_right().x.as_int();
@@ -422,6 +433,7 @@ TEST_F(BasicSurfaceTest, updates_default_input_region_when_surface_is_resized_to
 {
     geom::Rectangle const new_rect{rect.top_left,{2,2}};
     surface.resize(new_rect.size);
+    post_a_frame(surface);
 
     for (auto x = rect.top_left.x.as_int() - 1;
          x <= rect.top_right().x.as_int();
@@ -450,6 +462,8 @@ TEST_F(BasicSurfaceTest, restores_default_input_region_when_setting_empty_input_
     std::vector<geom::Rectangle> const rectangles = {
         {{geom::X{0}, geom::Y{0}}, {geom::Width{1}, geom::Height{1}}}, //region0
     };
+
+    post_a_frame(surface);
 
     surface.set_input_region(rectangles);
     EXPECT_FALSE(surface.input_area_contains(rect.bottom_right() - geom::Displacement{1,1}));
