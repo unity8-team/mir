7a38bdc97c52daecc1c55c8f86927c48aa66bd2d
William Hua <william.hua@canonical.com>
2015-01-23 09:59:21 -0500

    Use attachment_rect for menu surface position.

--- src/server/shell/default_placement_strategy.cpp
+++ src/server/shell/default_placement_strategy.cpp
@@ -43,7 +43,10 @@ ms::SurfaceCreationParameters msh::DefaultPlacementStrategy::place(
         mir_display_output_id_invalid};
     auto placed_parameters = request_parameters;
 
-    geom::Rectangle rect{request_parameters.top_left, request_parameters.size};
+    if (placed_parameters.attachment_rect.is_set())
+      placed_parameters.top_left = placed_parameters.attachment_rect.value().bottom_left();
+
+    geom::Rectangle rect{placed_parameters.top_left, placed_parameters.size};
 
     if (request_parameters.output_id != output_id_invalid)
     {
