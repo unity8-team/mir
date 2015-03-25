9aada4ca2f7c1ef566e4df2a37a0ab5e4dfb34ce
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2015-03-25 16:49:50 +1100

    Run protobuf_protocol.cpp through clang-format

--- src/server/frontend/protobuf_protocol.cpp
+++ src/server/frontend/protobuf_protocol.cpp
@@ -35,13 +35,12 @@ namespace mf = mir::frontend;
 namespace mfd = mir::frontend::detail;
 namespace ba = boost::asio;
 
-mf::ProtobufProtocol::ProtobufProtocol(
-    std::unique_ptr<ProtobufIpcFactory> ipc_factory,
-    std::shared_ptr<MessageProcessorReport> const& report)
-:   ipc_factory{std::move(ipc_factory)},
-    report(report),
-    next_session_id(0),
-    connections(std::make_shared<mfd::Connections<mfd::SocketConnection>>())
+mf::ProtobufProtocol::ProtobufProtocol(std::unique_ptr<ProtobufIpcFactory> ipc_factory,
+                                       std::shared_ptr<MessageProcessorReport> const& report)
+    : ipc_factory{std::move(ipc_factory)},
+      report(report),
+      next_session_id(0),
+      connections(std::make_shared<mfd::Connections<mfd::SocketConnection>>())
 {
 }
 
@@ -56,26 +55,24 @@ int mf::ProtobufProtocol::next_id()
 }
 
 void mf::ProtobufProtocol::create_connection_for(std::shared_ptr<ba::local::stream_protocol::socket> const& socket,
-                                                          SessionAuthorizer& authorizer,
-                                                          ConnectionContext const& connection_context,
-                                                          std::string const&)
+                                                 SessionAuthorizer& authorizer,
+                                                 ConnectionContext const& connection_context,
+                                                 std::string const&)
 {
     auto const messenger = std::make_shared<detail::SocketMessenger>(socket);
     auto const creds = messenger->client_creds();
 
     if (authorizer.connection_is_allowed(creds))
     {
-        auto const message_sender = std::make_shared<detail::ProtobufResponder>(
-            messenger,
-            ipc_factory->resource_cache());
+        auto const message_sender =
+            std::make_shared<detail::ProtobufResponder>(messenger, ipc_factory->resource_cache());
 
         auto const event_sink = std::make_shared<detail::EventSender>(messenger);
         auto const msg_processor = create_processor(
-            message_sender,
-            ipc_factory->make_ipc_server(authorizer, creds, event_sink, connection_context),
-            report);
+            message_sender, ipc_factory->make_ipc_server(authorizer, creds, event_sink, connection_context), report);
 
-        auto const& connection = std::make_shared<mfd::SocketConnection>(messenger, next_id(), connections, msg_processor);
+        auto const& connection =
+            std::make_shared<mfd::SocketConnection>(messenger, next_id(), connections, msg_processor);
         connections->add(connection);
         connection->read_next_message();
     }
@@ -86,14 +83,10 @@ mf::HandshakeProtocol& mf::ProtobufProtocol::connection_protocol()
     return connect_proto;
 }
 
-std::shared_ptr<mfd::MessageProcessor>
-mf::ProtobufProtocol::create_processor(
+std::shared_ptr<mfd::MessageProcessor> mf::ProtobufProtocol::create_processor(
     std::shared_ptr<mfd::ProtobufMessageSender> const& sender,
     std::shared_ptr<detail::DisplayServer> const& display_server,
     std::shared_ptr<mf::MessageProcessorReport> const& report) const
 {
-    return std::make_shared<detail::ProtobufMessageProcessor>(
-        sender,
-        display_server,
-        report);
+    return std::make_shared<detail::ProtobufMessageProcessor>(sender, display_server, report);
 }
