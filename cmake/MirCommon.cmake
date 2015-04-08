2bdb8debb70b447b5b9dbc996ffbd6e6ac13d69b
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2015-04-08 13:20:47 +1000

    Drop GTest test discovery.
    There's no particularly good reason to have two different ways of running the testsuite.
    Drop the one that isn't 3x slower on armhf CI due to valgrind process startup inefficiencies.

--- cmake/MirCommon.cmake
+++ cmake/MirCommon.cmake
@@ -1,15 +1,6 @@
 cmake_minimum_required (VERSION 2.6)
 # Create target to discover tests
 
-include(CMakeDependentOption)
-
-CMAKE_DEPENDENT_OPTION(
-  DISABLE_GTEST_TEST_DISCOVERY
-  "If set to ON, disables fancy test autodiscovery and switches back to classic add_test behavior"
-  OFF
-  "NOT CMAKE_CROSSCOMPILING"
-  ON)
-
 option(
   ENABLE_MEMCHECK_OPTION
   "If set to ON, enables automatic creation of memcheck targets"
@@ -44,112 +35,40 @@ if(ENABLE_MEMCHECK_OPTION)
 endif(ENABLE_MEMCHECK_OPTION)
 
 function (mir_discover_tests EXECUTABLE)
-  if(DISABLE_GTEST_TEST_DISCOVERY)
-    execute_process(
-      COMMAND uname -r
-      OUTPUT_VARIABLE KERNEL_VERSION_FULL
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    string(REGEX MATCH "^[0-9]+[.][0-9]+" KERNEL_VERSION ${KERNEL_VERSION_FULL})
-    message(STATUS "Kernel version detected: " ${KERNEL_VERSION})
-    # Some tests expect kernel version 3.11 and up
-    if (${KERNEL_VERSION} VERSION_LESS "3.11")
-        add_test(${EXECUTABLE} ${VALGRIND_EXECUTABLE} ${VALGRIND_ARGS} ${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE}
-            "--gtest_filter=-*DeathTest.*:AnonymousShmFile.*:MesaBufferAllocatorTest.software_buffers_dont_bypass:MesaBufferAllocatorTest.creates_software_rendering_buffer")
-    else()
-        add_test(${EXECUTABLE} ${VALGRIND_EXECUTABLE} ${VALGRIND_ARGS} ${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE}
-            "--gtest_filter=-*DeathTest.*")
-    endif()
-
-    add_test(${EXECUTABLE}_death_tests ${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE} "--gtest_filter=*DeathTest.*")
-    if (${ARGC} GREATER 1)
-      set_property(TEST ${EXECUTABLE} PROPERTY ENVIRONMENT ${ARGN})
-      set_property(TEST ${EXECUTABLE}_death_tests PROPERTY ENVIRONMENT ${ARGN})
-    endif()
+  execute_process(
+    COMMAND uname -r
+    OUTPUT_VARIABLE KERNEL_VERSION_FULL
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  string(REGEX MATCH "^[0-9]+[.][0-9]+" KERNEL_VERSION ${KERNEL_VERSION_FULL})
+  message(STATUS "Kernel version detected: " ${KERNEL_VERSION})
+  # Some tests expect kernel version 3.11 and up
+  if (${KERNEL_VERSION} VERSION_LESS "3.11")
+    add_test(${EXECUTABLE} ${VALGRIND_EXECUTABLE} ${VALGRIND_ARGS} ${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE}
+      "--gtest_filter=-*DeathTest.*:AnonymousShmFile.*:MesaBufferAllocatorTest.software_buffers_dont_bypass:MesaBufferAllocatorTest.creates_software_rendering_buffer")
   else()
-    set(CHECK_TEST_DISCOVERY_TARGET_NAME "check_discover_tests_in_${EXECUTABLE}")
-    set(TEST_DISCOVERY_TARGET_NAME "discover_tests_in_${EXECUTABLE}")
-    message(STATUS "Defining targets: ${CHECK_TEST_DISCOVERY_TARGET_NAME} and ${TEST_DISCOVERY_TARGET_NAME}")
+    add_test(${EXECUTABLE} ${VALGRIND_EXECUTABLE} ${VALGRIND_ARGS} ${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE}
+      "--gtest_filter=-*DeathTest.*")
+  endif()
 
-    # These targets are always considered out-of-date, and are always run (at least for normal builds, except for make test/install).
-    add_custom_target(
-      ${CHECK_TEST_DISCOVERY_TARGET_NAME} ALL
-      ${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE} --gtest_list_tests > /dev/null
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      COMMENT "Check that discovering Tests in ${EXECUTABLE} works")
-      
-    if (MIR_BUILD_PLATFORM_ANDROID)
-      add_dependencies(${CHECK_TEST_DISCOVERY_TARGET_NAME} mirplatformgraphicsandroid)
-    endif()
-    
-    if (MIR_BUILD_PLATFORM_MESA)
-      add_dependencies(${CHECK_TEST_DISCOVERY_TARGET_NAME} mirplatformgraphicsmesa)
-    endif()
-
-    if (${ARGC} GREATER 1)
-      foreach (env ${ARGN})
-        list(APPEND EXTRA_ENV_FLAGS "--add-environment" "${env}")
-      endforeach()
-    endif()
-    
-    if(cmake_build_type_lower MATCHES "threadsanitizer")
-        find_program(LLVM_SYMBOLIZER llvm-symbolizer-3.6)
-        if (LLVM_SYMBOLIZER)
-            set(TSAN_EXTRA_OPTIONS "external_symbolizer_path=${LLVM_SYMBOLIZER}")
-        endif()
-        list(APPEND EXTRA_ENV_FLAGS "--add-environment" "TSAN_OPTIONS=suppressions=${CMAKE_SOURCE_DIR}/tools/tsan-suppressions second_deadlock_stack=1 halt_on_error=1 history_size=7 ${TSAN_EXTRA_OPTIONS}")
-        # TSan does not support multi-threaded fork
-        # TSan may open fds so "surface_creation_does_not_leak_fds" will not work as written
-        # TSan deadlocks when running StreamTransportTest/0.SendsFullMessagesWhenInterrupted - disable it until understood
-        set(EXCLUDED_TESTS "UnresponsiveClient.does_not_hang_server:DemoInProcessServerWithStubClientPlatform.surface_creation_does_not_leak_fds:StreamTransportTest/0.SendsFullMessagesWhenInterrupted")
-    endif()
-
-    add_custom_target(
-      ${TEST_DISCOVERY_TARGET_NAME} ALL
-      ${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE} --gtest_list_tests | ${CMAKE_BINARY_DIR}/mir_gtest/mir_discover_gtest_tests --executable=${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE} --exclusions=${EXCLUDED_TESTS} ${DISCOVER_FLAGS}
-      ${EXTRA_ENV_FLAGS}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      COMMENT "Discovering Tests in ${EXECUTABLE}" VERBATIM)
-
-    add_dependencies(
-      ${CHECK_TEST_DISCOVERY_TARGET_NAME}
-      ${EXECUTABLE})
-
-    add_dependencies(
-      ${TEST_DISCOVERY_TARGET_NAME}
-
-      ${CHECK_TEST_DISCOVERY_TARGET_NAME}
-      ${EXECUTABLE}
-      mir_discover_gtest_tests)
-
+  add_test(${EXECUTABLE}_death_tests ${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE} "--gtest_filter=*DeathTest.*")
+  if (${ARGC} GREATER 1)
+    set_property(TEST ${EXECUTABLE} PROPERTY ENVIRONMENT ${ARGN})
+    set_property(TEST ${EXECUTABLE}_death_tests PROPERTY ENVIRONMENT ${ARGN})
   endif()
 endfunction ()
 
 function (mir_add_memcheck_test)
   if (ENABLE_MEMCHECK_OPTION)
-      if(DISABLE_GTEST_TEST_DISCOVERY)
-	add_custom_target(
-	  memcheck_test ALL
-	)
-	ADD_TEST("memcheck-test" ${CMAKE_BINARY_DIR}/mir_gtest/fail_on_success.sh ${VALGRIND_EXECUTABLE} ${VALGRIND_ARGS} ${CMAKE_BINARY_DIR}/mir_gtest/mir_test_memory_error)
-	add_dependencies(
-	  memcheck_test
+    add_custom_target(
+      memcheck_test ALL
+    )
+    ADD_TEST("memcheck-test" ${CMAKE_BINARY_DIR}/mir_gtest/fail_on_success.sh ${VALGRIND_EXECUTABLE} ${VALGRIND_ARGS} ${CMAKE_BINARY_DIR}/mir_gtest/mir_test_memory_error)
+    add_dependencies(
+      memcheck_test
 
-	  mir_test_memory_error
-	)
-      else()
-        add_custom_target(
-          memcheck_test ALL
-          ${CMAKE_BINARY_DIR}/mir_gtest/mir_discover_gtest_tests --executable=${CMAKE_BINARY_DIR}/mir_gtest/mir_test_memory_error --memcheck-test ${DISCOVER_FLAGS}
-          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-          COMMENT "Adding memcheck test" VERBATIM)
-
-        add_dependencies(
-          memcheck_test
-
-          mir_discover_gtest_tests
-          mir_test_memory_error)
-      endif()
+      mir_test_memory_error
+    )
   endif()
 endfunction()
 
