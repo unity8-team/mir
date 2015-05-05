2277b93f58d61080f9579168c1060e4d9d822e87
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2015-05-05 10:53:26 +1000

    Add a test to check for proper code formatting.
    This treats our .clang-format file as the authoritative source of style guidance,
    and checks that clang-format would format the file identically to how it is formatted.
    
    Because this test currently fails everywhere, it only checks explicitly whitelisted
    source files in tools/files-to-check-for-formatting. This is a plain text file that contains
    a list of filenames, one name per line, relative to the top source dir.

---
+++ tools/check-formatting.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e -x
+
+TEMPFILE=$(mktemp)
+
+# Clean up on exit...
+trap "rm \"$TEMPFILE\"" 0 1 2 3 15
+
+for file in $@ 
+do
+    clang-format -style=file $file > $TEMPFILE
+    (diff $file $TEMPFILE > /dev/null) || FAILED=""$FAILED" "$file
+done
+    
+if [ $FAILED ]
+then
+    for error in $FAILED
+    do
+	echo "File $error has formatting discrepency"
+    done
+    exit 1
+fi
+
+exit 0
